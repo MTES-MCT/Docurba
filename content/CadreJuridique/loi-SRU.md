308a97a469f701aa827e183afb4287216947b481
GuySake <fabien.ungerer@gmail.com>
2021-09-24 16:16:24 +0200

    first test for content edit

---
+++ content/CadreJuridique/loi-SRU.md
@@ -0,0 +1,23 @@
+---
+title: Loi SRU
+description: 'La loi du 13 décembre 2000 relative à la solidarité et au renouvellement urbain, appelée Loi SRU, est un texte complexe qui modifie en profondeur le droit de l’urbanisme et du logement en France. L’article 55 de cette loi impose à certaines communes de disposer d’un nombre minimum de logements sociaux.'
+---
+
+## Loi SRU
+
+La loi du 13 décembre 2000 relative à la solidarité et au renouvellement urbain, appelée Loi SRU, est un texte complexe qui modifie en profondeur le droit de l’urbanisme et du logement en France. L’article 55 de cette loi impose à certaines communes de disposer d’un nombre minimum de logements sociaux.
+
+La loi a été élaborée autour de trois exigences :
+- une plus grande solidarité,
+- l’encouragement au développement durable,
+- le renforcement de la démocratie et de la décentralisation.
+
+En particulier en **matière d’urbanisme**, les schémas directeurs (SD) sont remplacés par les schémas de cohérence territoriale (SCOT). Les plans d’occupation des sols (POS) sont remplacés par les plans locaux d’urbanisme (PLU). **On passe alors d’un urbanisme de zones à un urbanisme de projet.** Les plans d’aménagement de zone (PAZ) sont supprimés.
+
+En ce qui concerne **le patrimoine et plus spécifiquement la protection des monuments historiques**,le périmètre de protection de 500 mètres peut à présent, “sur proposition de l'architecte des Bâtiments de France et après accord de la commune, être modifié de façon à désigner des ensembles d'immeubles et des espaces qui participent de l'environnement du monument pour en préserver le caractère ou contribuer à en améliorer la qualité”.
+
+Concernant **la production de logements sociaux, l’article 55** de la loi SRU oblige certaines communes appartenant à des territoires bien intégrés et de taille suffisante pour mener une politique locale de l’habitat dynamique et volontaire, à disposer d’un nombre minimum de logements sociaux, proportionnel à leur parc résidentiel avec une obligation de 25 % ou 20 % de logements sociaux pour certaines communes.
+
+En ce qui concerne la commune de Crolles, soit “néant” soit renvoi vers la fiche thématique “Répondre 	aux besoins en matière de logements et en termes de mixité et de 	diversité”
+
+**Pour aller plus loin** : [https://www.cohesion-territoires.gouv.fr/loi-solidarite-et-renouvellement-urbain-sru](https://www.cohesion-territoires.gouv.fr/loi-solidarite-et-renouvellement-urbain-sru)
--- pages/pacsec.vue
+++ pages/pacsec.vue
@@ -1,99 +1,96 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="12">
-        <v-chip-group
-          v-model="selectedThemes"
-          column
-          multiple
-        >
-          <v-chip
-            v-for="themeKey in themesKeys"
-            :key="themeKey"
-            class="text-capitalize"
-            filter
-            outlined
-            color="bf500"
+    <v-tabs v-model="activeTab" show-arrows>
+      <v-tab
+        v-for="tab in tabs"
+        :key="tab"
+        :to="`#${tab}`"
+      >
+        {{ tab }}
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="activeTab">
+      <v-tab-item id="Cadre juridique">
+        <v-expansion-panels class="pa-4">
+          <v-expansion-panel
+            v-for="(item) in CadreJuridique"
+            :key="item.slug"
           >
-            {{ themeKey }}
-          </v-chip>
-        </v-chip-group>
-      </v-col>
-    </v-row>
-    <v-row v-for="(sources, theme) in filteredThemes" :key="theme">
-      <v-col cols="12">
-        <h2 class="text-h2">
-          {{ theme }}
-        </h2>
-      </v-col>
-      <v-col v-for="(source, i) in sources" :key="source.name" cols="4">
-        <v-card flat color="g100">
-          <v-card-title>
-            {{ source.name }}
-          </v-card-title>
-          <v-row v-if="source.subTheme">
-            <v-col cols="10">
-              <v-chip
-                outlined
-                class="text-capitalize"
-                color="bf300 ml-4"
-              >
-                {{ source.subTheme }}
-              </v-chip>
-            </v-col>
-            <v-col cols="2">
-              <v-btn
-                :href="i ? `https://www.data.gouv.fr/en/datasets/?q=${source.name}` : 'https://www.datara.gouv.fr/accueil/base_territoriale/'"
-                target="_blank"
-                icon
-              >
-                <v-icon small>
-                  {{ icons.mdiOpenInNew }}
-                </v-icon>
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-    </v-row>
+            <v-expansion-panel-header>
+              <v-row>
+                <v-col cols="12">
+                  <h3 class="text-h3">
+                    {{ item.title }}
+                  </h3>
+                </v-col>
+                <v-col cols="8">
+                  <p>
+                    {{ item.description }}
+                  </p>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <nuxt-content :document="item" />
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-tab-item>
+      <v-tab-item id="Documents supra-territoriaux">
+        <v-expansion-panels class="pa-4">
+          <v-expansion-panel
+            v-for="(item) in DocSupra"
+            :key="item.slug"
+          >
+            <v-expansion-panel-header>
+              <v-row>
+                <v-col cols="12">
+                  <h3 class="text-h3">
+                    {{ item.title }}
+                  </h3>
+                </v-col>
+                <v-col cols="8">
+                  <p>
+                    {{ item.description }}
+                  </p>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <nuxt-content :document="item" />
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-tab-item>
+      <v-tab-item id="Jeux de données">
+        <DataSourcesList />
+      </v-tab-item>
+    </v-tabs-items>
   </v-container>
 </template>
 
 <script>
-import { groupBy } from 'lodash'
-import { mdiOpenInNew } from '@mdi/js'
-import dataSources from '@/assets/data/DataSources.json'
 
 export default {
-  data () {
+  async asyncData ({ $content }) {
+    const CadreJuridique = await $content('CadreJuridique').fetch()
+    const DocSupra = await $content('DocumentsSupra-territoriaux').fetch()
+
     return {
-      dataSources,
-      selectedThemes: [],
-      icons: {
-        mdiOpenInNew
-      }
+      CadreJuridique,
+      DocSupra
     }
   },
-  computed: {
-    themes () {
-      return groupBy(dataSources, source => source.theme)
-    },
-    themesKeys () {
-      return Object.keys(this.themes).filter(k => k)
-    },
-    filteredThemes () {
-      if (this.selectedThemes.length) {
-        const themes = {}
-
-        this.selectedThemes.forEach((themeIndex) => {
-          const selectedTheme = this.themesKeys[themeIndex]
-          themes[selectedTheme] = this.themes[selectedTheme]
-        })
-
-        return themes
-      } else {
-        return this.themes
-      }
+  data () {
+    return {
+      tabs: [
+        'Cadre juridique',
+        'Documents supra-territoriaux',
+        // 'Politiques publiques spécifiques',
+        // 'Servitudes d’utilité publique',
+        'Jeux de données'
+      ],
+      activeTab: 'Cadre juridique'
     }
   }
 }
