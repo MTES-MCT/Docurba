7bfb99c97cbf5383f45e96571e8362ebf5a48584
GuySake <fabien.ungerer@gmail.com>
2022-03-11 10:19:26 +0100

    charte setup complete

--- layouts/print.vue
+++ layouts/print.vue
@@ -41,18 +41,28 @@ export default {
 }
 </script>
 
+<style>
+  /* We need both scoped and unscoped style for vuetify internal class */
+  .print-container .fr-header .v-toolbar__content {
+    margin-top: 1px;
+    padding: 0mm !important;
+  }
+
+  .print-container .v-main__wrap {
+    margin: 8.5mm 0mm
+  }
+</style>
+
 <style scoped>
 @page {
-  margin: 0mm;
+  margin: 17mm;
+  -webkit-print-color-adjust: exact !important;   /* Chrome, Safari, Edge */
+  color-adjust: exact !important;                 /*Firefox*/
 }
 
-.fr-header {
+/* .fr-header {
   padding: 17mm;
-}
-
-.fr-header .v-toolbar__content {
-  padding: 0mm;
-}
+} */
 
 .fr-header .fr-header__body-row, .fr-header .fr-header__logo, .fr-header .fr-logo {
   padding: 0mm !important;
@@ -65,6 +75,8 @@ export default {
 }
 
 .print-container {
-  padding: 17mm;
+  /* padding: 17mm; */
+  -webkit-print-color-adjust: exact !important;   /* Chrome, Safari, Edge */
+  color-adjust: exact !important;
 }
 </style>
