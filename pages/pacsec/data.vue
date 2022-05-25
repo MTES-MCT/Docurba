97c5821268db1db9ca30e8e5cd119a4393b9b322
GuySake <fabien.ungerer@gmail.com>
2022-05-25 17:27:18 +0200

    debug loading data when changing route

--- pages/pacsec/data.vue
+++ pages/pacsec/data.vue
@@ -1,22 +1,31 @@
 <template>
-  <DataSourcesList :region="currentRegion" :data-sources="dataset" :themes="themes" />
+  <DataSourcesList v-if="!loading" :region="currentRegion" :data-sources="dataset" :themes="themes" />
+  <VGlobalLoader v-else />
 </template>
 
 <script>
 export default {
-  async asyncData ({ $daturba }) {
-    return await $daturba.getData()
-  },
+  // async asyncData ({ $daturba, route }) {
+  //   return await $daturba.getData(route.query.region, route.query.insee)
+  // },
   data () {
     return {
       dataset: [],
-      themes: []
+      themes: [],
+      loading: true
     }
   },
   computed: {
     currentRegion () {
       return this.$route.query.region
     }
+  },
+  async mounted () {
+    const { dataset, themes } = await this.$daturba.getData(this.$route.query.region, this.$route.query.insee)
+
+    this.dataset = dataset
+    this.themes = themes
+    this.loading = false
   }
 }
 </script>
