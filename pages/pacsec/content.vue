bdee21647d29b2bdf5a1e787f8a88fb524707dc2
GuySake <fabien.ungerer@gmail.com>
2022-09-30 11:26:34 +0200

    filter sections based on docType

--- pages/pacsec/content.vue
+++ pages/pacsec/content.vue
@@ -7,17 +7,33 @@
 <script>
 
 export default {
-  async asyncData ({ $content }) {
-    const PAC = await $content('PAC', {
+  async asyncData ({ $content, route }) {
+    let PAC = await $content('PAC', {
       deep: true
     }).fetch()
 
     // remove unwanted intros
     PAC.splice(PAC.findIndex(s => s.path === '/PAC/Introduction/introcution-PAC-valide'), 1)
+    // Filter PP du territoire in socle
+    PAC = PAC.filter(s => !s.path.includes('PP-du-territoire'))
+    // Filter PLUi/PLU depending on doc type.
+    const docType = route.query.document
+    PAC = PAC.filter((s) => {
+      if (docType === 'PLU') {
+        // In PLU remove PLUi sections
+        return !s.titre.includes('PLUi')
+      } else if (docType.includes('PLUi')) {
+        // In PLUi remove PLU sections.
+        return !s.titre.includes(' PLU ')
+      } else {
+        // In CC remove all PLU/PLUi sections
+        return !s.titre.includes('PLU')
+      }
+    })
 
     return {
       // PAC: PAC.filter(s => !s.path.includes('olitiques-publiques-specifiques'))
-      PAC: PAC.filter(s => !s.path.includes('PP-du-territoire'))
+      PAC
     }
   },
   mounted () {
