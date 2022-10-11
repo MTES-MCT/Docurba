02935dd5cba8dca9606de5769ebebddf15ec0f24
GuySake <fabien.ungerer@gmail.com>
2022-10-11 16:15:01 +0200

    test events

--- components/PAC/ContentSection.vue
+++ components/PAC/ContentSection.vue
@@ -108,6 +108,7 @@ export default {
   },
   methods: {
     openSection (section) {
+      console.log('Test')
       // Start Analytics
       this.$matomo(['trackEvent', 'PAC Content', 'Open Section', section.titre])
       // End Analytics
