c1d17db151965cc238354c23d7dc01e67923b2f9
GuySake <fabien.ungerer@gmail.com>
2023-01-16 15:58:34 +0100

    all read routes

--- server-middleware/trames.js
+++ server-middleware/trames.js
@@ -11,13 +11,26 @@ app.post('/regions/:regionCode', async (req, res) => {
   const { userId, githubReq } = req.body
 
   const userRoles = await admin.getUserAdminRoles(userId)
-
   const isAllowed = !!userRoles.find(role => role.region === regionCode)
 
   if (isAllowed) {
     github(githubReq)
   } else {
     res.status(403).send(`User not allowed to edit region ${regionCode} trame.`)
+  }
+})
+
+app.post('/departement/:departementCode', async (req, res) => {
+  const { departementCode } = req.params
+  const { userId, githubReq } = req.body
+
+  const userRoles = await admin.getUserAdminRoles(userId)
+  const isAllowed = !!userRoles.find(role => role.dept === departementCode)
+
+  if (isAllowed) {
+    github(githubReq)
+  } else {
+    res.status(403).send(`User not allowed to edit region ${departementCode} trame.`)
   }
 })
 
@@ -32,6 +45,30 @@ app.get('/regions/:regionCode', async (req, res) => {
     // eslint-disable-next-line no-console
     console.log('error reading github:', error)
     req.status(400).send(error)
+  }
+})
+
+app.get('/departements/:departementCode/', async (req, res) => {
+  const { data, error } = await github('GET /repos/UngererFabien/France-PAC/contents/Trame?ref=test', {
+    path: 'Trame'
+  })
+
+  if (!error) {
+    res.status(200).send(data)
+  } else {
+    res.status(400).send(error)
+  }
+})
+
+app.get('/document/:documentId/', async (req, res) => {
+  const { data, error } = await github('GET /repos/UngererFabien/France-PAC/contents/Trame?ref=test', {
+    path: 'Trame'
+  })
+
+  if (!error) {
+    res.status(200).send(data)
+  } else {
+    res.status(400).send(error)
   }
 })
 
