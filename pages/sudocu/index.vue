5d109b2d0e28bc03ee6913f0acd985e1f11d855b
Julien Leray <leray.jul@gmail.com>
2023-01-17 11:55:30 +0100

    ok

---
+++ pages/sudocu/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <p class="text-h2">
+          Import des évènements Sudocu
+        </p>
+        <VTownAutocomplete v-model="selectedTown" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <FriseDocTimeline :events="events" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'Sudocu',
+  data () {
+    return {
+      selectedTown: null,
+      events: []
+    }
+  },
+  computed: {
+    formatEvents () {
+      return true
+    }
+  }
+}
+</script>
