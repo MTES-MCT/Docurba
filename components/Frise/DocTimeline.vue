4c85782165a157e81f5570abeb9ade7505433866
Julien Leray <leray.jul@gmail.com>
2023-01-19 18:35:39 +0100

    multi timeline d'import fonctionnelle

--- components/Frise/DocTimeline.vue
+++ components/Frise/DocTimeline.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row justify="end">
     <v-col cols="12">
-      <v-timeline align-top class="doc-timeline pt-0 mt-6">
+      <v-timeline v-if="!noProject" align-top class="doc-timeline pt-0 mt-6">
         <v-timeline-item color="#E3E3FD" small>
           <v-btn nuxt color="primary" :to="`/projets/${projectId}/frise/add`">
             Ajouter un événement
@@ -20,7 +20,7 @@
         </v-timeline-item>
       </v-timeline>
     </v-col>
-    <v-col offset="3" cols="9" class="pl-0">
+    <v-col v-if="!noProject" offset="3" cols="9" class="pl-0">
       <v-divider />
     </v-col>
     <v-col cols="12">
@@ -48,6 +48,10 @@ import documentEvents from '@/assets/data/DU_events.json'
 
 export default {
   props: {
+    noProject: {
+      type: Boolean,
+      default: () => false
+    },
     projectId: {
       type: String,
       default () {
--- pages/sudocu/index.vue
+++ pages/sudocu/index.vue
@@ -8,11 +8,22 @@
         <VTownAutocomplete v-model="selectedTown" />
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="12">
-        <FriseDocTimeline :events="events" />
-      </v-col>
-    </v-row>
+    <template v-if="!loading">
+      <v-row>
+        <v-col cols="12">
+          <v-tabs v-model="selectedDocType">
+            <v-tab v-for="docType in docTypes" :key="docType">
+              {{ docType }}
+            </v-tab>
+          </v-tabs>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <FriseDocTimeline :events="events[docTypes[selectedDocType]]" no-project />
+        </v-col>
+      </v-row>
+    </template>
   </v-container>
 </template>
 <script>
@@ -21,12 +32,41 @@ export default {
   data () {
     return {
       selectedTown: null,
+      selectedDocType: null,
+      loading: true,
       events: []
     }
   },
   computed: {
-    formatEvents () {
-      return true
+    docTypes () {
+      return Object.keys(this.events)
+    }
+  },
+  mounted () {
+    this.loadCommuneEvents()
+  },
+  methods: {
+    async loadCommuneEvents (commune) {
+      const rawEvents = (await this.$supabase.from('sudocu_events').select()).data
+      const formattedEvents = rawEvents.map((e) => {
+        return {
+          date_iso: e.dateevenement,
+          type: e.libtypeevenement,
+          description: '',
+          actors: [],
+          attachements: [],
+          docType: e.codetypedocument
+        }
+      })
+      this.events = formattedEvents.reduce(function (r, a) {
+        r[a.docType] = r[a.docType] || []
+        r[a.docType].push(a)
+        return r
+      }, Object.create(null))
+
+      this.selectedDocType = 0
+      console.log('test: ', this.events, ' this.selectedDocType: ', this.selectedDocType, ' selectedDocType: ', this.events[this.selectedDocType])
+      this.loading = false
     }
   }
 }
