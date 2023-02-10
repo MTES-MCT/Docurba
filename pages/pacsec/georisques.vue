779198edfa82184b626b57128953665daa73d94d
Julien Leray <leray.jul@gmail.com>
2023-02-10 11:45:27 +0100

    UI georisque ok

--- pages/pacsec/georisques.vue
+++ pages/pacsec/georisques.vue
@@ -1,80 +1,91 @@
 <template>
   <div>
-    <!-- <v-container>
-      <v-row>
-        <v-col cols="12">
-          <v-chip-group v-model="selectedTheme" column>
-            <v-chip
-              v-for="theme in themes"
-              :key="theme.endpoint"
-              class="text-capitalize"
-              :value="theme.endpoint"
-              filter
-              outlined
-              color="bf500"
-            >
-              {{ theme.name }}
-            </v-chip>
-          </v-chip-group>
-        </v-col>
-        <v-col cols="12">
-          {{ themes.find((e) => e.endpoint === selectedTheme).description }}
-        </v-col>
-      </v-row>
-    </v-container> -->
     <v-container>
       <v-row>
         <v-col cols="12">
           <h1>Risques Géographique pour {{ communes && communes.length > 1 ? 'l\'Epci' : 'la commune' }} </h1>
         </v-col>
+        <v-col cols="12">
+          <p>
+            Vous trouvez toutes les informations relatives aux risques sur le site georisques.gouv.fr.
+            Vous pouvez également télécharger directement la donnée du Plan Prévention des Risques directement depuis Docurba.
+          </p>
+        </v-col>
       </v-row>
       <v-row v-if="communes">
-        <v-col
-          v-for="(card, i) in communes"
-          :key="`${card.title}-${i}`"
-          cols="6"
-        >
-          <v-card flat outlined>
-            <v-card-title class="break-word">
-              Georisques {{ card.nom_commune }}
-            </v-card-title>
-            <v-card-text>
-              Vous trouvez toutes les informations relatives aux risques sur le site georisques.gouv.fr.
-              <br>
-              Vous pouvez également télécharger directement la donnée du Plan Prévention des Risques directement depuis Docurba.
-              <!-- {{ card.ppr }} -->
-            </v-card-text>
-            <v-card-actions>
-              <v-btn target="_blank" :href="card.urlGeoRisque" color="primary">
-                Voir les risques
-              </v-btn>
-              <v-btn color="primary" @click=" download (card.ppr, card.nom_commune)">
-                Télécharger les données PPR
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+        <v-col cols="12">
+          <v-data-table
+            :headers="headers"
+            :items="communes"
+            :items-per-page="10"
+            :footer-props="{'items-per-page-text':'commune par page'}"
+            class="elevation-1"
+          >
+            <template #[`footer.page-text`]="items">
+              {{ items.pageStart }} - {{ items.pageStop }} de {{ items.itemsLength }}
+            </template>
+
+            <!-- eslint-disable-next-line -->
+            <template #item.actions="{ item }">
+              <div class="">
+                <v-btn color="primary" depressed class="mr-4" target="_blank" :href="item.urlGeoRisque">
+                  <v-icon
+                    small
+                    class="mr-2"
+                  >
+                    {{ icons.mdiLink }}
+                  </v-icon>
+                  Voir les risques
+                </v-btn>
+                <!-- item.code_commune_INSEE, item.nom_commune -->
+                <v-btn color="primary" depressed :loading="item.loading" @click="download(item)">
+                  <v-icon
+                    small
+                    class="mr-2"
+                  >
+                    {{ icons.mdiDownload }}
+                  </v-icon>
+                  Télécharger les données PPR
+                </v-btn>
+              </div>
+            </template>
+          </v-data-table>
         </v-col>
       </v-row>
       <v-row v-else>
         <VGlobalLoader />
       </v-row>
     </v-container>
-    <!-- <GeoIDECardList
-      v-if="!loading"
-      :region="currentRegion"
-      :cards="dataset"
-      :themes="[]"
-    /> -->
-    <!-- <VGlobalLoader v-else /> -->
   </div>
 </template>
 
 <script>
+import { mdiLink, mdiDownload } from '@mdi/js'
 import GEORISQUES_MAP from '@/assets/data/GeoRisquesMap.json'
 
 export default {
+  name: 'Georisque',
   data () {
     return {
+      icons: {
+        mdiLink,
+        mdiDownload
+      },
+      headers: [
+        {
+          text: 'Commune',
+          value: 'nom_commune'
+        },
+        {
+          text: 'Code Postal',
+          value: 'code_postal'
+        },
+        {
+          text: 'Code INSEE',
+          value: 'code_commune_INSEE'
+        },
+        { text: 'Actions', value: 'actions', sortable: false, align: 'center' }
+      ],
       selectedTheme: GEORISQUES_MAP[0].endpoint,
       dataset: [],
       communes: null,
@@ -134,43 +145,41 @@ export default {
       Array.isArray(this.$route.query.insee) ? this.$route.query.insee : [this.$route.query.insee]
     )
 
-    const promsPpr = []
     communesData = communesData.map((e) => {
-      const pprData = this.$daturba.getGeorisques({
-        dataset: 'ppr',
-        insee: e.code_commune_INSEE
-      })
-      promsPpr.push(pprData)
       const enriched = {
         ...e,
-        urlGeoRisque: `https://www.georisques.gouv.fr/mes-risques/connaitre-les-risques-pres-de-chez-moi/rapport2?form-commune=true&codeInsee=${e.code_commune_INSEE}&city=${e.nom_commune}&typeForm=commune&postCode=${e.code_postal}`,
-        ppr: pprData
+        loading: false,
+        urlGeoRisque: `https://www.georisques.gouv.fr/mes-risques/connaitre-les-risques-pres-de-chez-moi/rapport2?form-commune=true&codeInsee=${e.code_commune_INSEE}&city=${e.nom_commune}&typeForm=commune&postCode=${e.code_postal}`
       }
       return enriched
     })
-    const pprs = await Promise.all(promsPpr)
-    communesData = communesData.map((e, i) => ({
-      ...e,
-      ppr: pprs[i]
-    }))
     this.communes = communesData
-
-    // await this.getTheme()
     this.loading = false
   },
   methods: {
-    download (jsonData, name) {
-      const text = JSON.stringify(jsonData)
-      const filename = `ppr_${name}.json`
-      const element = document.createElement('a')
-      element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text))
-      element.setAttribute('download', filename)
+    async download (commune) {
+      try {
+        commune.loading = true
+        const pprData = await this.$daturba.getGeorisques({
+          dataset: 'ppr',
+          insee: commune.code_commune_INSEE
+        })
+        console.log('pprData: ', pprData)
+        const text = JSON.stringify(pprData)
+        const filename = `ppr_${commune.nom_commune}.json`
+        const element = document.createElement('a')
+        element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text))
+        element.setAttribute('download', filename)
 
-      element.style.display = 'none'
-      document.body.appendChild(element)
+        element.style.display = 'none'
+        document.body.appendChild(element)
 
-      element.click()
-      document.body.removeChild(element)
+        element.click()
+        document.body.removeChild(element)
+        commune.loading = false
+      } catch (error) {
+        console.log('Error: ', error)
+      }
     },
     async getTheme () {
       function objToArr (obj) {
