511478149d50491844b9c42102204c9f832dd605
GuySake <fabien.ungerer@gmail.com>
2023-10-12 15:54:55 +0200

    rules should return a bool or it create a validator bug on strings

--- mixins/FormInput.js
+++ mixins/FormInput.js
@@ -30,7 +30,7 @@ extend('needToBeOui', {
 
 extend('requiredCollectivite', {
   validate (value) {
-    return (value.EPCI || value.code_commune_INSEE)
+    return !!(value.EPCI || value.code_commune_INSEE)
   },
   message: 'Vous devez choisir une collectivité'
 })
