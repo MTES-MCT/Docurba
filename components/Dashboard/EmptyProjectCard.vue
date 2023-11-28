a58f9eec9bfca737d8fc3a5e0454ac0bcb8e3ccf
GuySake <fabien.ungerer@gmail.com>
2023-11-28 17:32:05 +0100

    page mes pac v0

--- components/Dashboard/EmptyProjectCard.vue
+++ components/Dashboard/EmptyProjectCard.vue
@@ -2,22 +2,18 @@
   <v-card outlined>
     <v-card-text>
       <v-row>
-        <v-col cols="12" class="text-subtitle-1 font-weight-bold">
+        <v-col cols="" class="text-subtitle-1 font-weight-bold">
           <span>{{ project.doc_type }}</span>
           <br>
           <span>{{ project.name }}</span>
         </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          Aucune procédure existante pour ce {{ project.doc_type }}.
+        <v-spacer />
+        <v-col cols="auto" class="text-subtitle-1 font-weight-bold">
+          <span>{{ collectivity.intitule }} ({{ collectivity.code }})</span>
         </v-col>
       </v-row>
     </v-card-text>
     <v-card-actions>
-      <v-btn color="primary" tile disabled>
-        Ajouter une procedure
-      </v-btn>
       <v-btn color="primary" tile depressed nuxt :to="`/trames/projet-${project.id}`">
         Modifier le PAC
       </v-btn>
@@ -39,6 +35,10 @@
 export default {
   props: {
     project: {
+      type: Object,
+      required: true
+    },
+    collectivity: {
       type: Object,
       required: true
     }
