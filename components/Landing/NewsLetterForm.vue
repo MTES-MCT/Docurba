6e3fd2da14466e69565aba5f34c4c6b1ccd23e26
Nicolas Jorif <nico@MacBook-Pro-de-Nicolas.local>
2023-12-28 03:40:03 +0100

    landing footer

--- components/Landing/NewsLetterForm.vue
+++ components/Landing/NewsLetterForm.vue
@@ -1,50 +1,33 @@
 <template>
-  <v-container>
-    <v-row justify="center" align="center">
-      <v-col cols="12" md="5">
-        <h3 class="text-h5">
-          <b>Abonnez-vous à notre lettre d’information</b>
-        </h3>
-        <p class="mt-2">
-          Suivez l’avancée de nos travaux au fur et à mesure.Rythme d’environ une par mois.
-        </p>
-      </v-col>
-      <v-col cols="12" md="6" md-offset="1">
-        <v-row no-gutters>
-          <v-col cols="">
-            <v-text-field
-              v-model="email"
-              hide-details
-              filled
-              dense
-              placeholder="Votre adresse électronique (ex. : nom@domaine.fr)"
-              :loading="loading"
-              @click:append="sendEmail"
-            />
-          </v-col>
-          <v-col cols="auto">
-            <v-btn
-              color="primary"
-              depressed
-              height="41px"
-              tile
-              @click="sendEmail"
-            >
-              S'abonner
-            </v-btn>
-          </v-col>
-          <v-col cols="12">
-            <p class="text-caption mt-2">
-              En renseignant votre adresse électronique, vous acceptez de recevoir nos actualités par courriel. Vous pouvez vous désinscrire à tout moment à l’aide des liens de désinscription ou en nous contactant.
-            </p>
-          </v-col>
-        </v-row>
-      </v-col>
-      <!-- <v-col cols="auto">
-        <v-btn depressed tile>S'inscrire à la news letter</v-btn>
-      </v-col> -->
-    </v-row>
-  </v-container>
+  <v-row>
+    <v-col cols="9">
+      <v-text-field
+        v-model="email"
+        hide-details
+        filled
+        dense
+        placeholder="Votre adresse électronique (ex. : nom@domaine.fr)"
+        :loading="loading"
+        @click:append="sendEmail"
+      />
+    </v-col>
+    <v-col cols="auto">
+      <v-btn
+        color="primary"
+        depressed
+        height="41px"
+        tile
+        @click="sendEmail"
+      >
+        S'abonner
+      </v-btn>
+    </v-col>
+    <v-col cols="12">
+      <p class="text-caption mt-2">
+        En renseignant votre adresse électronique, vous acceptez de recevoir nos actualités par courriel. Vous pouvez vous désinscrire à tout moment à l’aide des liens de désinscription ou en nous contactant.
+      </p>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
--- components/Layouts/Footer.vue
+++ components/Layouts/Footer.vue
@@ -1,28 +1,51 @@
 <template>
-  <v-footer app absolute inset color="white">
+  <v-footer app absolute color="white pt-12">
     <v-container>
       <v-row>
-        <v-col>
+        <v-col cols="12" md="6">
           <h5>Ressources</h5>
-          <ul>
-            <li />
-            <li />
-            <li />
+          <ul :style="{ listStyle: 'none' }" class="pa-0 mt-4">
+            <li>
+              <a href="https://pad.incubateur.net/s/MVwW7cGNh#Nos-Guides-d%E2%80%99utilisation" _target="blank">Guides d'utilisation</a>
+            </li>
+            <li>
+              <a href="https://docurba.beta.gouv.fr/faq">FAQ</a>
+            </li>
+            <li>
+              <a href="https://pad.incubateur.net/s/MVwW7cGNh#Nos-webinaires">Webinaires</a>
+            </li>
+            <li>
+              <nuxt-link to="/accessibilite">Accessibilité</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/confidentialite">Politique de confidentialité</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/mentions-legales">Mentions légales</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/stats">Statistiques</nuxt-link>
+            </li>
           </ul>
         </v-col>
-        <v-col>
-          <v-container fluid>
-            <v-row>
-              <v-col>
-                Newsletter
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                Réseaux sociaux
-              </v-col>
-            </v-row>
-          </v-container>
+        <v-col cols="12" md="6">
+          <v-row>
+            <v-col>
+              <h5>Newsletter</h5>
+              <p class="mt-4">Suivez l’avancée de Docurba tous les mois.</p>
+              <LandingNewsLetterForm />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <h5>Réseaux sociaux</h5>
+              <div class="mt-4 d-flex" :style="{ gap: '1rem' }">
+                <a href="https://www.linkedin.com/company/docurba/" target="_blank">
+                  <v-img max-width="40px" height="40px" :src="require('@/assets/images/icons/linkedin.svg')" />
+                </a>
+              </div>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
@@ -32,5 +55,10 @@
 <style scoped>
 h5 {
   font-size: 22px;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
 }
 </style>
