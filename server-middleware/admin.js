76e10718e70ad96183fccafe362fe068b46fce44
GuySake <fabien.ungerer@gmail.com>
2023-12-28 15:47:20 +0100

    added supabase modules and firstname lastname in help request mail

--- server-middleware/admin.js
+++ server-middleware/admin.js
@@ -4,20 +4,33 @@ app.use(express.json())
 
 const sendgrid = require('./modules/sendgrid.js')
 const slack = require('./modules/slack.js')
+const supabase = require('./modules/supabase.js')
 
-app.post('/help', (req, res) => {
+app.post('/help', async (req, res) => {
   const { title, message, section, email, dir } = req.body
 
+  // Send an email to the team.
   sendgrid.sendEmail({
     to: ['fabien@quantedsquare.com', 'celia.vermicelli@beta.gouv.fr', 'celia.vermicelli@docurba.beta.gouv.fr', 'hermance.gauthier@docurba.beta.gouv.fr'],
-    cc: [email],
     replyTo: email,
-    template_id: 'd-23a3309075ab4710af6028e4639bf6dc',
+    template_id: 'd-c0c2bbdfc40546bbaa2e0c0f167d02f3',
     dynamic_template_data: {
       title,
       message,
       section,
       dir
+    }
+  })
+
+  const { data: profile } = await supabase.from('profiles').select('firstname, lastname').eq('email', email)
+
+  // Send a confirmation email to the user.
+  sendgrid.sendEmail({
+    to: email,
+    template_id: 'd-23a3309075ab4710af6028e4639bf6dc',
+    dynamic_template_data: {
+      firstname: profile.firstname,
+      lastname: profile.lastname
     }
   })
 
