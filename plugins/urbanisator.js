88840d7519c35925ac4afb31e27932b3fbf30f16
GuySake <fabien.ungerer@gmail.com>
2024-03-12 17:38:49 +0100

    clean read procedures

--- plugins/urbanisator.js
+++ plugins/urbanisator.js
@@ -19,32 +19,6 @@ export default ({ route, store, $supabase, $user, $dayjs, $sudocu }, inject) => 
   })
 
   inject('urbanisator', {
-    isEpci (collectiviteId) {
-      return collectiviteId.toString().length > 5
-    },
-    getRegionDetails (regionCode) {
-      if (typeof regionCode === 'number') { regionCode = regionCode.toString() }
-      return regions.find(e => e.code.toString().padStart(2, '0') === regionCode)
-    },
-    async getCurrentCollectivite (collectiviteId) {
-      try {
-        const { data: collectivite } = await axios({
-          url: `/api/${this.isEpci(collectiviteId) ? 'epci' : 'communes'}/${collectiviteId}`,
-          method: 'get'
-        })
-
-        const isEpci = this.isEpci(collectiviteId)
-        collectivite.id = isEpci ? collectivite.EPCI : collectivite.code_commune_INSEE.toString().padStart(5, '0')
-        collectivite.name = isEpci ? collectivite.label : collectivite.nom_commune
-        collectivite.type = isEpci ? 'epci' : 'commune'
-        const regionCode = isEpci ? collectivite.towns[0].code_region : collectivite.code_region
-        collectivite.region = this.getRegionDetails(regionCode)
-        return collectivite
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log('Error getCurrentCollectivite: ', error)
-      }
-    },
     async getCollectivite (code) {
       if (code.length > 5) {
         return (await axios({ url: `/api/geo/intercommunalites/${code}`, method: 'get' })).data
@@ -52,86 +26,63 @@ export default ({ route, store, $supabase, $user, $dayjs, $sudocu }, inject) => 
         return (await axios({ url: `/api/geo/communes/${code}`, method: 'get' })).data
       }
     },
-    async getProjectsProcedures (collectiviteId) {
-      // Fetching user procedure for now. Should also fetch public projects at some point.
-      // Or more simply fetch based on collectivite column but need to make a script to update projects.
-      const ret = {
-        projects: [],
-        procedures: []
-      }
+    async getCommuneProcedures (inseeCode) {
+      const { data: perimetre } = await $supabase.from('procedures_perimetres').select('*').eq('collectivite_code', inseeCode)
+      const { data: procedures } = await $supabase.from('procedures_duplicate')
+        .select('*, projects(*)').eq('archived', false)
+        .in('id', perimetre.map(p => p.procedure_id))
 
-      const { data: projects } = await $supabase.from('projects').select('id, name, doc_type, towns, collectivite_id, PAC, trame, region').match({
-        owner: $user.id, // TODO: fetch shared projects.
-        collectivite_id: collectiviteId,
-        archived: false
+      return procedures
+    },
+    async getIntercoProcedures (collectiviteId) {
+      const { data: procedures } = await $supabase.from('procedures_duplicate')
+        .select('*, projects(*)').eq('archived', false)
+        .eq('collectivite_porteuse_id', collectiviteId)
+
+      return procedures
+    },
+    async getProceduresPerimetre (procedures) {
+      const { data: perimetre } = await $supabase.from('procedures_perimetres').select('*')
+        .in('procedure_id', procedures.map(p => p.id))
+
+      procedures.forEach((procedure) => {
+        // This is to manage legacy structure
+        procedure.current_perimetre = perimetre.filter(p => p.procedure_id)
+        procedure.current_perimetre.forEach((c) => {
+          c.inseeCode = c.collectivite_code
+        })
       })
 
-      ret.projects = projects ?? []
-      // console.log('projects: ', projects)
-      // TODO: Should be a join, no need to double select
-      if (projects) {
-        const projectsIds = projects.map(p => p.id)
-        const { data: procedures } = await $supabase.from('procedures').select('*').in('project_id', projectsIds)
-
-        procedures.forEach((procedure) => {
-          procedure.project = projects.find(p => p.id === procedure.project_id)
-
-          procedure.procSecs = procedures.filter(proc => proc.procedure_id === procedure.id)
-        })
-
-        projects.forEach((project) => {
-          project.procedures = procedures.filter((proc) => {
-            return proc.project_id === project.id
-          })
-        })
-
-        ret.procedures = procedures ?? []
-      }
-      return ret
+      return procedures
     },
-    async getProjects (collectiviteId, { plans = true, schemas = true } = {}) {
+    async getProjects (collectiviteId) {
       try {
-        // eslint-disable-next-line no-console
-        console.log('Fetch: ', collectiviteId)
-        let query = $supabase.from('procedures')
-          .select('*, projects(*)').eq('archived', false)
+        let procedures = []
 
         if (collectiviteId.length > 5) {
-          query = query.eq('collectivite_porteuse_id', collectiviteId)
+          procedures = await this.getIntercoProcedures(collectiviteId)
         } else {
-          query = query.contains('initial_perimetre', `[{ "inseeCode": "${collectiviteId}" }]`)
+          procedures = await this.getCommuneProcedures(collectiviteId)
         }
 
-        if (schemas && !plans) {
-          query = query.eq('doc_type', 'SCOT')
-        }
-        if (plans && !schemas) {
-          query = query.neq('doc_type', 'SCOT')
-        }
-        const { data, error } = await query.order('created_at', { ascending: false })
-        if (error) {
-          // eslint-disable-next-line no-console
-          console.log('urbanisator.getProjects error', error)
-          throw error
-        }
+        procedures = await this.getProceduresPerimetre(procedures)
+        const groupedSubProcedures = groupBy(procedures, 'secondary_procedure_of')
 
-        const groupedSubProcedures = groupBy(data, 'secondary_procedure_of')
-
-        const proceduresPrincipales = data.filter(e => e.is_principale)
-          .map((e) => {
-            const { projects, ...rest } = e
-            return { ...rest, project: projects, procSecs: groupedSubProcedures[e.id] }
+        const proceduresPrincipales = procedures.filter(p => p.is_principale)
+          .map((p) => {
+            const { projects, ...rest } = p
+            return { ...rest, project: projects, procSecs: groupedSubProcedures[p.id] }
           })
 
-        const ret = {}
-        if (schemas) { ret.schemas = proceduresPrincipales.filter(e => e.doc_type === 'SCOT') }
-        if (plans) { ret.plans = proceduresPrincipales.filter(e => e.doc_type !== 'SCOT') }
+        const schemas = proceduresPrincipales.filter(e => e.doc_type === 'SCOT')
+        const plans = proceduresPrincipales.filter(e => e.doc_type !== 'SCOT')
+
         // eslint-disable-next-line no-console
-        console.log('ret: ', ret)
-        return ret
+        console.log('urbanisator get projects', { schemas, plans })
+        return { schemas, plans }
       } catch (error) {
         // eslint-disable-next-line no-console
-        console.log(error)
+        console.log('urbanisator error', error)
       }
     }
   })
