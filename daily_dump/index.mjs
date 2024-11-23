ecdae824d58423cac4f352f4313169a17d73f290
Anthony Ricaud <anthony@ricaud.me>
2024-11-23 19:14:19 +0100

    Import - Arrêtons le script après une erreur
    ref https://github.com/MTES-MCT/Docurba/issues/893

--- daily_dump/index.mjs
+++ daily_dump/index.mjs
@@ -21,42 +21,38 @@ await downloadDump(CONFIG.PG_PROD_CONFIG, DUMP_FILENAME, __dirname)
 // You need to setup .pgpass in your home if you don't want to enter the db password on pg_restaure
 // https://tableplus.com/blog/2019/09/how-to-use-pgpass-in-postgresql.html
 
-try {
-  /// ////////////////////////////////////////
-  /// ///// PART 1 - Prepare the data ////////
-  /// ////////////////////////////////////////
+/// ////////////////////////////////////////
+/// ///// PART 1 - Prepare the data ////////
+/// ////////////////////////////////////////
 
-  // Step 0 (Optionnal)
-  await clearDev(CONFIG.PG_DEV_CONFIG)
-  // // // // // Step 1 - Charge un dump particulier venant de l'export de Andy sur notre storage
+// Step 0 (Optionnal)
+await clearDev(CONFIG.PG_DEV_CONFIG)
+// // // // // Step 1 - Charge un dump particulier venant de l'export de Andy sur notre storage
 
-  await loadDump(CONFIG.PG_DEV_CONFIG, DUMP_FILENAME)
+await loadDump(CONFIG.PG_DEV_CONFIG, DUMP_FILENAME)
 
-  // // // // // // Step 2 - Créer les tables intermédiaires d'aggregation depuis la donnée Sudocuh
-  await createSudocuProcessedTables(CONFIG.PG_DEV_CONFIG)
-  // // // // // // Replique un schema de test (Optionnal)
-  await createOriginalSchema(CONFIG.PG_DEV_CONFIG)
-  // // // // // // Step 3 - Désactive du trigger de changement de status sur nouveaux events
-  await handleTrigger(CONFIG.PG_PROD_CONFIG, 'disable')
+// // // // // // Step 2 - Créer les tables intermédiaires d'aggregation depuis la donnée Sudocuh
+await createSudocuProcessedTables(CONFIG.PG_DEV_CONFIG)
+// // // // // // Replique un schema de test (Optionnal)
+await createOriginalSchema(CONFIG.PG_DEV_CONFIG)
+// // // // // // Step 3 - Désactive du trigger de changement de status sur nouveaux events
+await handleTrigger(CONFIG.PG_PROD_CONFIG, 'disable')
 
-  // // /// /////////////////////////////////////////////////////////////
-  // // /// ///// PART 2 - Sudocuh to Docurba daily differential ////////
-  // // /// /////////////////////////////////////////////////////////////
+// // /// /////////////////////////////////////////////////////////////
+// // /// ///// PART 2 - Sudocuh to Docurba daily differential ////////
+// // /// /////////////////////////////////////////////////////////////
 
-  // // // Step 4 - Migre les nouvelles données plan entrées dans Sudocuh dans Docurba
-  await sudocuhPlanToDocurba(CONFIG.PG_DEV_CONFIG, CONFIG.PG_PROD_CONFIG)
-  // // // // Step 5 - Migre les nouvelles données SCoT entrées dans Sudocuh dans Docurba
-  await sudocuhScotToDocurba(CONFIG.PG_DEV_CONFIG, CONFIG.PG_PROD_CONFIG)
-  // // // // Step 6 - Définition des status de procédures au niveau event
-  await setAllStatus(CONFIG.PG_PROD_CONFIG)
-  // // Step 6(Bis) - Définition des status de procédures en fonction des périmètres
-  await updatePerimeterStatus(CONFIG.PG_PROD_CONFIG)
-  // // Step 7 - Ralliement des communes fusionnées
-  // await updateComDPerimeter(CONFIG.PG_PROD_CONFIG)
-  //
-  await migrateDgd(CONFIG.PG_DEV_CONFIG, CONFIG.PG_PROD_CONFIG)
-  // // Step 8 - Réactivation du trigger de changement de status sur nouveaux events
-  await handleTrigger(CONFIG.PG_PROD_CONFIG, 'enable')
-} catch (error) {
-  console.log(error)
-}
+// // // Step 4 - Migre les nouvelles données plan entrées dans Sudocuh dans Docurba
+await sudocuhPlanToDocurba(CONFIG.PG_DEV_CONFIG, CONFIG.PG_PROD_CONFIG)
+// // // // Step 5 - Migre les nouvelles données SCoT entrées dans Sudocuh dans Docurba
+await sudocuhScotToDocurba(CONFIG.PG_DEV_CONFIG, CONFIG.PG_PROD_CONFIG)
+// // // // Step 6 - Définition des status de procédures au niveau event
+await setAllStatus(CONFIG.PG_PROD_CONFIG)
+// // Step 6(Bis) - Définition des status de procédures en fonction des périmètres
+await updatePerimeterStatus(CONFIG.PG_PROD_CONFIG)
+// // Step 7 - Ralliement des communes fusionnées
+// await updateComDPerimeter(CONFIG.PG_PROD_CONFIG)
+//
+await migrateDgd(CONFIG.PG_DEV_CONFIG, CONFIG.PG_PROD_CONFIG)
+// // Step 8 - Réactivation du trigger de changement de status sur nouveaux events
+await handleTrigger(CONFIG.PG_PROD_CONFIG, 'enable')
