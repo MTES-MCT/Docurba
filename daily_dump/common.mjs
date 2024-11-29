89e5f4ff5f888a9d1e5a21b7a37d9d2dfe601c70
Anthony Ricaud <anthony@ricaud.me>
2024-11-29 21:41:21 +0100

    Import - Saut de ligne après chaque message du résumé
    Et correction d'une typo
    
    ref https://github.com/MTES-MCT/Docurba/issues/893

--- daily_dump/common.mjs
+++ daily_dump/common.mjs
@@ -1,4 +1,4 @@
 import { appendFileSync } from 'node:fs'
 export function appendToGithubSummary(markdown) {
-  appendFileSync(process.env.GITHUB_STEP_SUMMARY, markdown)
+  appendFileSync(process.env.GITHUB_STEP_SUMMARY, markdown + '\n')
 }
