8b9471605d40c399a962e148708776063172cbbd
Anthony Ricaud <anthony@ricaud.me>
2024-12-30 21:34:55 +0100

    Nettoyage review

--- pages/ddt/enquete_overview.vue
+++ pages/ddt/enquete_overview.vue
@@ -5,28 +5,25 @@
       <v-col cols="12">
         <h1>Validation des procédures - Aperçu</h1>
       </v-col>
-      <!-- <v-col v-if="!collectivites" cols="12">
-        <v-skeleton-loader
-          type="table"
-        />
-      </v-col> -->
       <v-col cols="12">
         <v-data-table
           :headers="headers"
           :items="deptsItems"
-          :sort-by="['departement']"
+          :items-per-page="-1"
+          :sort-by="['departement_code']"
           :sort-desc="[false]"
-          :custom-sort="customSort"
           must-sort
+          style="font-variant-numeric: tabular-nums;"
         >
           <!-- eslint-disable-next-line -->
-        <template #item.departement="{ item }">
+        <template #item.departement_code="{ item }">
             <div>{{ item.departement_code }} - {{ item.departement_intitule }}</div>
           </template>
 
           <!-- eslint-disable-next-line -->
-        <template #item.collectivites="{ item }">
-            <span class="primary--text font-weight-bold">{{ item.nb_validated }}</span> <span class="mention-grey--text"> / {{ item.nb_collectivites }}</span>
+        <template #item.nb_validated="{ item }">
+            <span class="primary--text font-weight-bold">{{ item.nb_validated }}</span>
+            <span class="mention-grey--text"> / {{ item.nb_collectivites.toString().padStart(3,'0') }}</span>
           </template>
 
           <!-- eslint-disable-next-line -->
@@ -49,7 +46,6 @@
 </template>
 
 <script>
-import axios from 'axios'
 import { mdiArrowRight } from '@mdi/js'
 
 export default {
@@ -66,64 +62,38 @@ export default {
   computed: {
     headers () {
       return [
-        {
-          text: 'Département',
-          align: 'start',
-          value: 'departement'
-        },
-        { text: 'Collectivités', align: 'start', value: 'collectivites' },
-        { text: 'Restantes', align: 'start', value: 'restantes', sortable: true },
-        { text: '% validées', align: 'start', value: 'percentage', sortable: true },
+        { text: 'Département', align: 'start', value: 'departement_code' },
+        { text: 'Collectivités', align: 'end', value: 'nb_validated' },
+        { text: 'Restantes', align: 'end', value: 'restantes' },
+        { text: '% validées', align: 'end', value: 'percentage' },
         { text: '', value: 'action' }
       ]
     }
   },
   async mounted () {
-    const { data: collectivitesByDepts } = await axios('/json/collectivites_by_department_enriched.json')
-    this.deptsItems = collectivitesByDepts
-    const { data: nbValidationByDepts, error } = await this.$supabase.rpc('validated_collectivites_by_depts_2024')
-    console.log('error: ', error)
+    if (!this.$user.profile.is_admin) {
+      return
+    }
 
-    const validationsMap = new Map(
+    const response = await fetch('/json/nb_collectivites_par_departement.json')
+    const nbCollectivitesParDepartement = await response.json()
+
+    const { data: nbValidationByDepts } = await this.$supabase.rpc('validated_collectivites_by_depts_2024').throwOnError()
+
+    const nbValidationByDeptsMap = new Map(
       nbValidationByDepts?.map(item => [item.departement, item.unique_collectivites_count]) || []
     )
 
     // Enrich deptsItems with nb_validated
-    this.deptsItems = collectivitesByDepts.map((dept) => {
-      const nbValidated = validationsMap.get(dept.departement_code) || 0
+    this.deptsItems = nbCollectivitesParDepartement.map((departement) => {
+      const nbValidated = nbValidationByDeptsMap.get(departement.departement_code) || 0
       return {
-        ...dept,
+        ...departement,
         nb_validated: nbValidated,
-        restantes: dept.nb_collectivites - nbValidated,
-        percentage: Math.round((nbValidated / dept.nb_collectivites) * 100)
+        restantes: departement.nb_collectivites - nbValidated,
+        percentage: Math.round((nbValidated / departement.nb_collectivites) * 100)
       }
     })
-  },
-  methods: {
-    customSort (items, sortBy, sortDesc) {
-      if (sortBy.length === 0) { return items }
-
-      const direction = sortDesc[0] ? -1 : 1
-
-      return items.sort((a, b) => {
-        if (sortBy[0] === 'departement') {
-          const codeA = a.departement_code.padStart(2, '0')
-          const codeB = b.departement_code.padStart(2, '0')
-          return direction * codeA.localeCompare(codeB)
-        }
-        if (sortBy[0] === 'collectivites') {
-          return direction * (a.nb_validated - b.nb_validated)
-        }
-        if (sortBy[0] === 'restantes') {
-          return direction * (a.restantes - b.restantes)
-        }
-        if (sortBy[0] === 'percentage') {
-          return direction * (a.percentage - b.percentage)
-        }
-        // Add other sort cases if needed
-        return 0
-      })
-    }
   }
 }
 </script>
