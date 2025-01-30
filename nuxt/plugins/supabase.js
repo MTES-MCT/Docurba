10e1d19f8e960bc7e72be6ebb123995472851d0e
Fabien Ungerer <fabien.ungerer@gmail.com>
2025-01-30 15:50:45 +0100

    supabase auth token in cookies

--- nuxt/plugins/supabase.js
+++ nuxt/plugins/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 // import { PG_DEV_CONFIG, PG_PROD_CONFIG } from '@/database/pg_secret_config.json'
 
 export default (_, inject) => {
@@ -6,7 +6,7 @@ export default (_, inject) => {
   const supabaseUrl = 'https://supabase.docurba.beta.gouv.fr'
   const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4eGJ5dWFuZGJtcGxmbnF0eHl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NTA4ODMsImV4cCI6MjA1NjQyNjg4M30.UDomMAiu5MZrQ8NqfDBfSrXRL_O3dSIrP8pyGy6QTAc'
 
-  inject('supabase', createClient(supabaseUrl, supabaseAnonKey))
+  inject('supabase', createBrowserClient(supabaseUrl, supabaseAnonKey))
   // const DB_CONFIG = PG_DEV_CONFIG
   // console.log(`Running on ${DB_CONFIG.env === 'dev' ? 'Dev' : 'Prod'} database instance.`)
   // console.log('Need run with PG_PROD_CONFIG for deploy: ', PG_PROD_CONFIG)
