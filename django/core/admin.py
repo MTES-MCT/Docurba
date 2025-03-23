49e789ccbb4e925372d49bb4985fed039ea3a5de
Anthony Ricaud <anthony@ricaud.me>
2025-03-23 18:01:07 +0100

    API périmètres en Django
    - Pour l'API :
        - Modélisation minimale permettant de générer cette API sans utiliser la colonne opposabilité de la table `procedures_perimetres`
        - Ajout de la vue pour cette API
        - Ajout d'une vue rudimentaire pour voir l'état des procédures d'une collectivité
        - Django admin rudimentaire
        - Mise à jour de la documentation pour refléter la nouvelle API
    - Ajustement de la config Django
        - Ajout des extensions django_browser_reload, django_extensions et debug_toolbar pour faciliter le développement
        - Ajout du middleware GZipMiddleware pour compresser les réponses
        - Gestion des statiques avec Whitenoise
        - Configuration de la base de données
        - Désactivation de https en local
        - Configuration de pytest
        - Configuration de ruff
    
    ref https://github.com/MTES-MCT/Docurba/issues/1227

---
+++ django/core/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from core.models import CommuneProcedure, Procedure
+
+
+class CommuneProcedureInline(admin.StackedInline):
+    model = CommuneProcedure
+    extra = 0
+    readonly_fields = ("id", "created_at")
+
+
+@admin.register(Procedure)
+class ProcedureAdmin(admin.ModelAdmin):
+    readonly_fields = ("id", "created_at")
+    list_filter = (
+        "is_principale",
+        ("name", admin.EmptyFieldListFilter),
+    )
+    list_display = ("__str__", "statut")
+    inlines = (CommuneProcedureInline,)
--- django/core/urls.py
+++ django/core/urls.py
@@ -1,7 +1,17 @@
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
-from django.urls import re_path
+from django.contrib import admin
+from django.urls import include, path, re_path
 from revproxy.views import ProxyView
 
+from core import views
+
 urlpatterns = [
+    path("_admin/", admin.site.urls),
+    path("collectivite/<collectivite_code>/", views.collectivite),
+    path("collectivite/<collectivite_code>/<collectivite_type>/", views.collectivite),
+    path("api/perimetres", views.api_perimetres),
+    path("__reload__/", include("django_browser_reload.urls")),
+    *debug_toolbar_urls(),
     re_path(r"(?P<path>.*)", ProxyView.as_view(upstream=settings.UPSTREAM_NUXT)),
 ]
