4256af433388de208b4c2941f61c08946ab03327
Anthony Ricaud <anthony@ricaud.me>
2025-03-24 14:24:00 +0100

    Form création procédure - Refactoring vérification de droits
    ref https://github.com/MTES-MCT/Docurba/issues/1258

--- nuxt/pages/collectivites/_collectiviteId/procedures/add.vue
+++ nuxt/pages/collectivites/_collectiviteId/procedures/add.vue
@@ -34,12 +34,18 @@ export default {
     const collectivites = await this.$nuxt3api(`/api/geo/search/collectivites?code=${this.$route.params.collectiviteId}&populate=true`)
     this.collectivite = collectivites[0]
 
-    if (this.$user.profile?.side === 'etat') {
-      if (this.$user.profile?.departement !== this.collectivite.departementCode) {
-        this.$router.push(`/collectivites/${this.$route.params.collectiviteId}`)
-      }
-    } else if (this.$user.profile?.collectivite_id !== this.collectivite.code &&
-          this.$user.profile?.collectivite_id !== this.collectivite.intercommunaliteCode) {
+    const isSideEtatWithMatchingDepartement = this.$user.profile.side === 'etat' &&
+      this.$user.profile.departement === this.collectivite.departementCode
+    const isSideCollectiviteWithMatchingCollectivite = this.$user.profile.side === 'collectivite' &&
+      (this.$user.profile.collectivite_id === this.collectivite.code ||
+      this.$user.profile.collectivite_id === this.collectivite.intercommunaliteCode)
+
+    const canCreateProcedure =
+      isSideEtatWithMatchingDepartement ||
+      isSideCollectiviteWithMatchingCollectivite
+
+    if (!canCreateProcedure) {
+      console.warn('Pas assez de droits pour créer une procédure sur ce périmètre')
       this.$router.push(`/collectivites/${this.$route.params.collectiviteId}`)
     }
   }
