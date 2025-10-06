e12ed736813f58487659f2de7281f9f6a40b0cac
Anthony Ricaud <anthony@ricaud.me>
2025-10-06 15:45:14 +0200

    Restreint les URLs que Django transmet à Nuxt
    Plutôt que de transmettre toutes les URLs non connues par Django à Nuxt, nous ajoutons dans le routeur Django la liste des préfixes supportés par Nuxt.
    
    En attendant d'avoir le DSFR dans Django, la page 404 générée par Nuxt est hardcodée dans un template Django.
    
    fix https://github.com/MTES-MCT/Docurba/issues/1511

--- django/core/urls.py
+++ django/core/urls.py
@@ -7,6 +7,8 @@ from revproxy.views import ProxyView
 
 from core import views
 
+nuxt_proxy = ProxyView.as_view(upstream=settings.UPSTREAM_NUXT)
+
 urlpatterns = [
     path("_admin/", admin.site.urls),
     path("collectivite/<collectivite_code>/", views.collectivite),
@@ -20,8 +22,38 @@ urlpatterns = [
     path("api/scots", views.api_scots, name="api_scots"),
     path("__reload__/", include("django_browser_reload.urls")),
     *debug_toolbar_urls(),
+    # URLs Nuxt globales
+    path("", nuxt_proxy, {"path": ""}),
     path(  # Désactive le websocket hot reload Nuxt en dev
         "_content/ws", lambda _: HttpResponseNotFound()
     ),
-    re_path(r"(?P<path>.*)", ProxyView.as_view(upstream=settings.UPSTREAM_NUXT)),
+    re_path(r"(?P<path>^_content.*)", nuxt_proxy),
+    re_path(r"(?P<path>^_nuxt.*)", nuxt_proxy),
+    re_path(r"(?P<path>^api.*)", nuxt_proxy),
+    # URLs Nuxt dans nuxt/static
+    re_path(r"(?P<path>^favicon.*)", nuxt_proxy),
+    re_path(r"(?P<path>^fonts.*)", nuxt_proxy),
+    re_path(r"(?P<path>^images.*)", nuxt_proxy),
+    re_path(r"(?P<path>^json.*)", nuxt_proxy),
+    re_path(r"(?P<path>^pdf.*)", nuxt_proxy),
+    re_path(r"(?P<path>^ressources.*)", nuxt_proxy),
+    re_path(r"(?P<path>^sw.js)", nuxt_proxy),
+    # URLs Nuxt trouvées via this.$router.options.routes
+    re_path(r"(?P<path>^accessibilite.*)", nuxt_proxy),
+    re_path(r"(?P<path>^bureau-etude-urbanisme.*)", nuxt_proxy),
+    re_path(r"(?P<path>^collectivites.*)", nuxt_proxy),
+    re_path(r"(?P<path>^confidentialite.*)", nuxt_proxy),
+    re_path(r"(?P<path>^ddt.*)", nuxt_proxy),
+    re_path(r"(?P<path>^dev.*)", nuxt_proxy),
+    re_path(r"(?P<path>^documents.*)", nuxt_proxy),
+    re_path(r"(?P<path>^exports.*)", nuxt_proxy),
+    re_path(r"(?P<path>^frise.*)", nuxt_proxy),
+    re_path(r"(?P<path>^guide.*)", nuxt_proxy),
+    re_path(r"(?P<path>^login.*)", nuxt_proxy),
+    re_path(r"(?P<path>^loi-climat-et-resilience.*)", nuxt_proxy),
+    re_path(r"(?P<path>^mentions-legales.*)", nuxt_proxy),
+    re_path(r"(?P<path>^print.*)", nuxt_proxy),
+    re_path(r"(?P<path>^stats.*)", nuxt_proxy),
+    re_path(r"(?P<path>^trames.*)", nuxt_proxy),
+    re_path(r"(?P<path>^validation.*)", nuxt_proxy),
 ]
