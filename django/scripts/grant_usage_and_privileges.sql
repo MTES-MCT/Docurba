9606f8bf5628bfd33647b8df0d7af092fa8a59e0
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2025-10-08 19:49:41 +0200

    Système de sauvegarde et de restauration de la base de données
    ref https://github.com/MTES-MCT/Docurba/issues/1465

---
+++ django/scripts/grant_usage_and_privileges.sql
@@ -0,0 +1,15 @@
+-- Obligatoire pour toute nouvelle branche Supabase.
+-- https://stackoverflow.com/a/73107153
+grant usage on schema public to postgres, anon, authenticated, service_role;
+grant usage on schema extensions to postgres, anon, authenticated, service_role;
+
+grant all privileges on all tables in schema public to postgres, anon, authenticated, service_role, supabase_admin;
+grant all privileges on all functions in schema public to postgres, anon, authenticated, service_role, supabase_admin;
+grant all privileges on all sequences in schema public to postgres, anon, authenticated, service_role, supabase_admin;
+
+alter default privileges in schema public grant all on tables to postgres, anon, authenticated, service_role;
+alter default privileges in schema public grant all on functions to postgres, anon, authenticated, service_role;
+alter default privileges in schema public grant all on sequences to postgres, anon, authenticated, service_role;
+
+alter role anon set statement_timeout = '3s';
+alter role authenticated set statement_timeout = '8s';
---
+++ django/scripts/restore_database.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# Usage :
+# ./restore_database postgresql://user:password@host.name:port/database
+
+# Lancement du script en mode strict (non officiel).
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+mkdir -p ${BACKUPS_FOLDER_PATH}
+database_restoration_url=$1
+
+if [[ ! -n ${database_restoration_url} ]]; then
+  echo "Il manque l'URL de la base de données. Fin du script."
+  exit 0
+fi
+
+if [[ ! -n ${RCLONE_S3_ACCESS_KEY_ID} ]]; then
+  echo "Ce script a besoin que Rclone soit correctement configuré."
+  echo "Vérifiez que vous avez toutes les variables d'environnement requises."
+  echo "Fin du script."
+  exit 0
+fi
+
+if type rclone 2>/dev/null; then
+  echo "Rclone est déjà installé."
+else
+  wget https://downloads.rclone.org/rclone-current-linux-amd64.zip
+  unzip rclone-current-linux-amd64.zip
+  mv rclone-*-linux-amd64/rclone rclone
+  chmod +x rclone
+  export PATH="${PWD}:${PATH}"
+fi
+
+rclone_last_backup="$(rclone lsf --files-only --max-age 48h docurba:/docurba-backups | head -n 1)"
+rclone copy --max-age 24h "docurba:/docurba-backups/${rclone_last_backup}" "${BACKUPS_FOLDER_PATH}"
+
+# Le fichier compressé est supprimé lors de la décompression.
+gzip --decompress "${BACKUPS_FOLDER_PATH}/${rclone_last_backup}"
+
+files_count=$(ls -1 ${BACKUPS_FOLDER_PATH} | wc -l)
+if [[ ${files_count} -gt 1 ]]; then
+  echo "Plus d'un fichier de sauvegarde décompressé mais un seul est attendu."
+  echo "Arrêt du script."
+  exit 0
+fi
+
+backup_file="${BACKUPS_FOLDER_PATH}/$(ls ${BACKUPS_FOLDER_PATH} | head -1)"
+echo "${backup_file} téléchargé et décompressé correctement."
+
+# Suppression des chaînes de caractère "https://" ou "http://" pour éviter de réaliser
+# des requêtes HTTP vers l'environnement de production par erreur.
+# En effet, plusieurs déclencheurs (triggers) ont été configurés pour réaliser des appels
+# HTTP vers l'URL de production.
+# Voir par exemple  https://github.com/MTES-MCT/Docurba/blob/main/nuxt/daily_dump/steps/sql/status_handler/4-trigger_event_procedure_status_handler.sql#L19
+# On ne peut pas simplement supprimer le type de connexion car la fonction `http_get` le remplace
+# s'il est manquant. Mais la fonction `net.http_get`, utilisée dans les déclencheurs, ne le remplace pas.
+# Dans le doute, je supprime également le premier caractère qui suit le type de connexion.
+# Cela peut également remplacer des URL présentes dans les données mais
+# j'estime que le fonctionnement est déjà satisfaisant pour une première version.
+if [[ ${BACKUPS_DELETE_URL_IN_DUMP_FILE} == "True" ]]; then
+  echo "Suppression des URL dans le fichier de sauvegarde."
+  sed --expression 's/\(https:\/\/.\|http:\/\/.\)//g' --in-place ${backup_file} # Prend environ 10s.
+  echo "URL supprimées."
+fi
+
+echo "Début de la restauration."
+# Comme décrit dans la documentation de Supabase.
+# J'ai supprimé l'option `--variable ON_ERROR_STOP=1` pour restaurer malgré des erreurs.
+# `session_replication_role` est déjà ajouté lors de la sauvegarde je ne souhaite
+# prendre aucun risque avec les déclencheurs.
+# https://www.postgresql.org/docs/15/runtime-config-client.html
+# Impossible d'utiliser `pg_restore` avec une archive _plain text_.
+psql \
+  --command 'SET session_replication_role = replica' \
+  --file ${backup_file} \
+  --dbname ${database_restoration_url}
+
+# https://supabase.com/docs/guides/troubleshooting/refresh-postgrest-schema
+psql \
+  --command "NOTIFY pgrst, 'reload schema';"\
+  --dbname ${database_restoration_url}
+
+if [[ ${BACKUPS_SUPABASE_GRANT_PRIVILEGES_TO_USERS} == "True" ]]; then
+  psql \
+    --file "${HOME}/scripts/grant_usage_and_privileges.sql" \
+    --dbname ${database_restoration_url}
+fi
+echo "La restauration est terminée !"
+
+# Ne conservons pas une copie locale trop longtemps.
+rm ${backup_file}
+echo "Le fichier de sauvegarde est supprimé. Fin du script."
