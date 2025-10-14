da433a1b701a5a03bdd78df7c15789cd4a18771a
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2025-10-14 18:57:44 +0200

    Nuxt : remplacer URL et clé anon par variables d'env
    Travail préparatoire aux recettes jetables, à la démo et
    à l'environnement de dev local.
    
    https://v2.nuxt.com/docs/configuration-glossary/configuration-env#automatic-injection-of-environment-variables

--- nuxt/server-middleware/modules/supabase.js
+++ nuxt/server-middleware/modules/supabase.js
@@ -3,7 +3,7 @@ const { createClient } = require('@supabase/supabase-js')
 // 'https://drncrjteathtblggsgxi.supabase.co'
 // PROD
 // 'https://ixxbyuandbmplfnqtxyw.supabase.co'
-const supabase = createClient('https://ixxbyuandbmplfnqtxyw.supabase.co', process.env.SUPABASE_ADMIN_KEY, {
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ADMIN_KEY, {
   auth: { persistSession: false }
 })
 
