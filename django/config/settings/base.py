d07a477039b6aa9bd54b11211a053a83ec1b9f90
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2025-11-21 14:02:17 +0100

    Django: django-filter installation
    Il s'agit de la bibliothèque recommandée par DRF pour ajouter des
    filtres aux requêtes.

--- django/config/settings/base.py
+++ django/config/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "revproxy.apps.RevProxyConfig",
     "docurba.core",
     "docurba.surveys",
