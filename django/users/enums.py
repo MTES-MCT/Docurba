be0e4ebc954a84a5d54272c73532d41a89661832
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2025-12-02 17:06:50 +0100

    Django : ajout des modèles User et Profile
    Documentation du schéma actuel de la base de données.
    Ajout d'une application pour distinguer la gestion des
    utilisateurs du reste de l'applicatif.
    Ajout des entrées dans l'interface d'administration Django.

---
+++ django/users/enums.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class ProfileSideType(models.TextChoices):
+    ETAT = "etat", "État"
+    COLLECTIVITE = "collectivite", "Collectivité"
+
+
+class PosteType(models.TextChoices):
+    # Postes État
+    # https://github.com/MTES-MCT/Docurba/blob/2050c8dc046f9c8414ebe9aaf72a3f3cdbc623d2/nuxt/plugins/utils.js#L58
+    DDT = "ddt", "DDT(M)/DEAL"
+    DREAL = "dreal", "DREAL"
+
+    # Postes Collectivités
+    BE = "be", "Bureau d'études"
+    ELU = "elu", "Collectivité, Élu·e"
+    EMPLOYE_MAIRIE = "employe_mairie", "Collectivité, Technicien·ne ou employé·e"
+    AGENCE_URBA = "agence_urba", "Agence d'urbanisme"
+    AUTRE = "autre", "Autre"
+
+
+class RoleType(models.TextChoices):
+    # Nuxt :
+    # Côté collectivité, c'est un InputField qui ne valide pas l'input.
+    # A contrario, côté État, c'est un TextChoices avec un menu déroulant. U_U
+    # https://github.com/MTES-MCT/Docurba/blob/2050c8dc046f9c8414ebe9aaf72a3f3cdbc623d2/nuxt/plugins/utils.js#L62
+    # Cela étant, il semblerait que les utilisateurs n'aient pas été très inspirés car seules 33
+    # valeurs différentes figurent en base (en excluant les comptes tests), dont des "PPA".
+    # Rôles état
+    CHEF_UNITE = "chef_unite", "Chef·fe d'unité/de bureau/de service et adjoint·e"
+    REDACTEUR_PAC = "redacteur_pac", "Rédacteur·ice de PAC"
+    SUIVI_PROCEDURES = (
+        "suivi_procedures",
+        "Chargé·e de l'accompagnement des collectivités",
+    )
+    REFERENT_SUDOCUH = "referent_sudocuh", "Référent·e Sudocuh"
