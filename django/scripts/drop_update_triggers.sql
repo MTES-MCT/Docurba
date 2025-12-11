4378d2cb9e373bdd1c5004c94a936a8ad4aeb5fc
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2025-12-11 15:05:16 +0100

    [à fixup] MàJ script SQL et séparation des étapes

--- django/scripts/drop_update_triggers.sql
+++ django/scripts/drop_update_triggers.sql
@@ -1,9 +1,11 @@
--- Mise à jour de la colonne `updated_at` lors de chaque modification.
+-- Activation des extensions.
 create extension if not exists moddatetime schema extensions;
-DROP TRIGGER IF EXISTS "handle_updated_at" ON public.doc_frise_events;
-create trigger handle_updated_at before update on public.doc_frise_events
-  for each row execute procedure moddatetime (updated_at);
 
 -- Pipedrive n'est pas configuré dans les recettes jetables.
 DROP TRIGGER IF EXISTS "Pipedrive Sharing Update" ON public.projects_sharing;
 DROP TRIGGER IF EXISTS "Pipedrive Update" ON public.profiles;
+
+-- Mise à jour de la colonne `updated_at` lors de chaque modification.
+DROP TRIGGER IF EXISTS "handle_updated_at" ON public.doc_frise_events;
+create trigger handle_updated_at before update on public.doc_frise_events
+  for each row execute procedure moddatetime (updated_at);
