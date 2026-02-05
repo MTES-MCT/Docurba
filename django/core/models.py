d7630ee9f5c76475ce1a58cd11f3d962331f4eca
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-02-05 15:49:40 +0100

    Django admin: give procedures_perimetres class representation a meaningful name

--- django/core/models.py
+++ django/core/models.py
@@ -877,6 +877,7 @@ class CommuneProcedure(models.Model):  # noqa: DJ008
     class Meta:
         managed = False
         db_table = "procedures_perimetres"
+        verbose_name = "Périmètre"
 
 
 class ViewCommuneAdhesionsDeep(models.Model):  # noqa: DJ008
