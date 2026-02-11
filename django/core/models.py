b602b16695b12c5cd66c765eb3960109f782294e
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-02-11 21:18:45 +0100

    Django: try to fix the N+1 query - third attempt

--- django/core/models.py
+++ django/core/models.py
@@ -487,15 +487,6 @@ class Procedure(models.Model):
     @property
     def type_document(self) -> TypeDocument:
         if self.doc_type in (TypeDocument.PLU, *PLU_LIKE):
-            # self.perimetre_count is set when calling Procedure.objects.with_communes_counts
-            # which is always called on the manager (see ProcedureManager.get_queryset).
-            # For the moment, it is mandatory to know if the procedure is_intercommunal.
-            # The type_document is used to generate the self representation, thus on the Django admin.
-            # For an unknown reason, the ProcedureManager is used on the "list" view but not on the "change" one.
-            # This should be refactored some day as this hack is quite ugly.
-            if not hasattr(self, "perimetre__count"):
-                self.perimetre__count = self.perimetre.count()
-
             if not self.is_intercommunal:
                 return TypeDocument.PLU
             if self.vaut_PLH_consolide and self.vaut_PDM_consolide:
