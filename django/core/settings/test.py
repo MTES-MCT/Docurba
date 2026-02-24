e95f309d58da588cc1e848b6e7c3a20ebf600ef0
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-02-24 16:51:51 +0100

    Django: split settings
    Default settings are production ones but some environments need a specific configuration.
    For example, the Django debug toolbar should only be activated in dev.

---
+++ django/core/settings/test.py
@@ -0,0 +1,8 @@
+# ruff: noqa: F405 F403
+from core.settings.base import *  # NOSONAR (S2208)
+
+STORAGES["staticfiles"]["BACKEND"] = (
+    "django.contrib.staticfiles.storage.StaticFilesStorage"
+)
+SESSION_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
