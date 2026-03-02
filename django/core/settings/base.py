eab45c276599607f37f3170acca5aa20cb878e77
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-03-02 10:52:14 +0100

    Django: enable Sentry's profiling feature
    Start profiling memroy usage to find out where our
    API memory problems come from.

--- django/core/settings/base.py
+++ django/core/settings/base.py
@@ -156,7 +156,14 @@ LOGGING = {
 #####################################################
 sentry_sdk.init(
     dsn=env.str("SENTRY_DSN", default=""),
-    traces_sample_rate=1,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # To set a uniform sample rate
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
 )
 
 ##########################################
