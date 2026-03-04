8efd7aa208e0e021f8f63eac4e4b8823a6645651
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-03-04 19:14:33 +0100

    FIXME - enhance the migrate_sudocuh_comments script

--- django/tests/core/management/commands/test_migrate_sudocuh_comments.py
+++ django/tests/core/management/commands/test_migrate_sudocuh_comments.py
@@ -2,16 +2,16 @@ import csv
 from pathlib import Path
 
 import pytest
-from django.conf import settings
 from django.core.management import call_command
+from pytest_django.fixtures import SettingsWrapper
 
 from docurba.core.models import Procedure
 
 
 @pytest.mark.django_db
 class TestMigrateSudocuhComments:
-    def _assert_csv_output(self) -> None:
-        with Path.open(Path(settings.EXPORTS_DIR) / "sudocuh_comments.csv") as csvfile:
+    def _assert_csv_output(self, filepath: Path) -> None:
+        with Path.open(filepath) as csvfile:
             results = list(csv.DictReader(csvfile, delimiter=";"))
             assert len(results) == 4
             row = results[0]
@@ -30,7 +30,14 @@ class TestMigrateSudocuhComments:
             "Updated procedures: 4",
         ]
 
-    def test_call_command_dry_run(self, capsys: pytest.CaptureFixture[str]) -> None:
+    def test_call_command_dry_run(
+        self,
+        capsys: pytest.CaptureFixture[str],
+        settings: SettingsWrapper,
+        tmp_path: Path,
+    ) -> None:
+        settings.EXPORTS_DIR = tmp_path
+        filepath = Path(settings.EXPORTS_DIR) / "sudocuh_comments.csv"
         for i in range(4):
             Procedure.objects.create(
                 commentaire=f"{i} Un commentaire très intéressant.", from_sudocuh=i
@@ -42,12 +49,19 @@ class TestMigrateSudocuhComments:
 
         call_command("migrate_sudocuh_comments", wet_run=False)
         stdout, _ = capsys.readouterr()
-        self._assert_csv_output()
+        self._assert_csv_output(filepath=filepath)
         self._assert_stdout(stdout)
         assert Procedure.objects.filter(commentaire__isnull=False).count() == 5
         assert Procedure.objects.filter(comment_from_sudocuh="").count() == 6
 
-    def test_call_command_wet_run(self, capsys: pytest.CaptureFixture[str]) -> None:
+    def test_call_command_wet_run(
+        self,
+        capsys: pytest.CaptureFixture[str],
+        settings: SettingsWrapper,
+        tmp_path: Path,
+    ) -> None:
+        settings.EXPORTS_DIR = tmp_path
+        filepath = Path(settings.EXPORTS_DIR) / "sudocuh_comments.csv"
         for i in range(4):
             Procedure.objects.create(
                 commentaire=f"{i} Un commentaire très intéressant.", from_sudocuh=i
@@ -55,7 +69,7 @@ class TestMigrateSudocuhComments:
 
         call_command("migrate_sudocuh_comments", wet_run=True)
         stdout, _ = capsys.readouterr()
-        self._assert_csv_output()
+        self._assert_csv_output(filepath=filepath)
         self._assert_stdout(stdout)
         assert Procedure.objects.filter(commentaire="").count() == 4
         assert Procedure.objects.filter(comment_from_sudocuh="").count() == 0
