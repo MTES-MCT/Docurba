592051a6d60f6ea0b0f7f8abaef4d665ca17b1c9
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-03-05 15:08:37 +0100

    Django: prototype a survey feature
    We reuse the `enquete` feature present in Nuxt but changing
    the way results are stored.
    This storage may evolve in the future as this is just a quick
    proposal to display the survey next week.

--- django/docurba/core/models.py
+++ django/docurba/core/models.py
@@ -992,3 +992,54 @@ class ViewCommuneAdhesionsDeep(models.Model):  # noqa: DJ008
         """Uniquement pour les tests."""
         with connection.cursor() as cursor:
             cursor.execute(f"REFRESH MATERIALIZED VIEW {cls._meta.db_table}")
+
+
+class ProcedureSurvey(models.Model):
+    procedure = models.ForeignKey(
+        "core.Procedure", on_delete=models.CASCADE, related_name="surveys_answers"
+    )
+    survey = models.ForeignKey(
+        "core.Survey", on_delete=models.RESTRICT, related_name="procedures_through"
+    )
+    respondant = models.ForeignKey(
+        "users.Profile",
+        on_delete=models.RESTRICT,
+        related_name="surveys_answers",
+        null=True,
+    )
+    created_at = models.DateTimeField("créé le", auto_now_add=True)
+    responded_at = models.DateTimeField("répondu le", auto_now=True)
+
+    # Despite its name, it's a SIREN or an INSEE.
+    collectivite_code = models.ForeignKey(
+        "core.Collectivite",
+        models.DO_NOTHING,
+        verbose_name="Code collectivité",
+        to_field="code_insee_unique",
+    )
+
+    is_validated = models.BooleanField("est validée", null=True)
+
+    class Meta:
+        verbose_name = "validation"
+        unique_together = ("procedure", "survey")
+
+    def __str__(self) -> str:
+        return f"{self.pk}"
+
+
+class Survey(models.Model):
+    name = models.CharField(verbose_name="nom unique", unique=True)
+    procedures = models.ManyToManyField(
+        "core.Procedure",
+        through="ProcedureSurvey",
+        related_name="surveys",
+        verbose_name="procédures",
+    )
+    created_at = models.DateTimeField("créée le", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "enquête"
+
+    def __str__(self) -> str:
+        return self.name
--- django/tests/core/test_models.py
+++ django/tests/core/test_models.py
@@ -15,6 +15,8 @@ from docurba.core.models import (
     Event,
     EventCategory,
     Procedure,
+    ProcedureSurvey,
+    Survey,
     Topic,
     TypeDocument,
     ViewCommuneAdhesionsDeep,
@@ -24,6 +26,7 @@ from tests.factories import (
     create_departement,
     create_groupement,
 )
+from tests.users.factories import create_user_and_profile
 
 
 class TestCollectivite:
@@ -1814,3 +1817,20 @@ class TestCommuneCodeEtat:
 class TestTopic:
     def test_fixtures(self) -> None:
         assert Topic.objects.count() == 5
+
+
+@pytest.mark.django_db
+class TestSurveys:
+    def test_fixtures(self) -> None:
+        assert Survey.objects.count() == 1
+        _, profile = create_user_and_profile(
+            email="georges-eugene@haussmann.com", other_poste=["rédacteur", "maire"]
+        )
+        survey = Survey.objects.filter(name="zan_03_2026").first()
+        procedure = Procedure.objects.create()
+        ProcedureSurvey.objects.create(
+            survey=survey, procedure=procedure, respondant=profile
+        )
+        assert procedure.surveys_answers.count() == 1
+        assert profile.surveys_answers.count() == 1
+        assert survey.procedures_through.count() == 1
