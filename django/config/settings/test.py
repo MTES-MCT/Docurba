c663063a4253b36ff7233fc4babc9fcec23706ec
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-04-01 12:05:19 +0200

    Tooling: Run tests on a production-like database
    The database, created by Pytest on a local PG server, was missing
    Supabase's specific structure used in production. For example,
    the `auth` schema with the `users` table was first created by a
    conftest.py and then by a signal.
    It was also missing tables, functions and triggers created manually
    on Supabase before Django migrations were used.
    The more we import in Django the production structure, the more
    we need a database that acts like the real one.
    
    The test database is now created in a Docker container whose image
    is managed directly by Supabase. See the `docker-compose.yml`.
    The file `prod_schema_before_django_migrations.sql` references how the
    database structure was before we start to use Django. It is used as a seed
    file to populate the test database.
    A mise command starts all the above and run migrations (start:tests).
    Then Pytest uses the test database (thanks to the tests config).

--- django/config/settings/test.py
+++ django/config/settings/test.py
@@ -7,3 +7,17 @@ STORAGES["staticfiles"]["BACKEND"] = (
 SESSION_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
 CREATE_UNMANAGED_TABLES = True
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("TEST_PGDATABASE", "test_postgres"),
+        "USER": env.str("TEST_PGUSER", "postgres"),
+        "CONN_MAX_AGE": 3600,
+        "PASSWORD": env.str("TEST_PGPASSWORD", "postgres"),
+        "HOST": env.str("TEST_PGHOST", "127.0.0.1"),
+        "PORT": env.str("TEST_PGPORT", "5431"),
+        "TEST": {
+            "MIRROR": "default",  # ✅ Forces Django to use the existing database
+        },
+    },
+}
