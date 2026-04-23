5c714e73b8cba7f017c61298f6557efdf62df571
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2026-04-23 17:26:13 +0200

    Fix sonar & retours review

--- docker/test_db/setup_test_db.sh
+++ docker/test_db/setup_test_db.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)/../../
 
 docker compose up test_db -d
 RETRIES=15
-until docker compose exec test_db bash -c '(psql -c "select 1 from public.only_exists_in_test;") > /dev/null 2>&1' || [ ${RETRIES} -eq 0 ]; do
+until docker compose exec test_db bash -c '(psql -c "select 1 from public.only_exists_in_test;") > /dev/null 2>&1' || [[ ${RETRIES} -eq 0 ]]; do
     echo "Waiting for Postgres server, $((RETRIES--)) remaining attempts..."
     sleep 2
 done
