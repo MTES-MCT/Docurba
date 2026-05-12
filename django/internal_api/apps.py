40cd89fb62d62f3e9d660de1f78fa6227a711ccb
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2026-05-12 11:00:31 +0200

    Create django application internal_api

---
+++ django/internal_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InternalApiConfig(AppConfig):
+    name = "internal_api"
+    verbose_name = "API internes"
---
+++ django/internal_api/urls.py
@@ -0,0 +1,7 @@
+from rest_framework import routers
+
+app_name = "internal_api"
+
+router = routers.DefaultRouter()
+
+urlpatterns = router.urls
