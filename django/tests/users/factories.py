45a42e588506cd7a8c2f8746e89485365358ea80
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2026-05-13 10:20:52 +0200

    Add email field to ProfileFactory

--- django/tests/users/factories.py
+++ django/tests/users/factories.py
@@ -24,7 +24,8 @@ class ProfileFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Profile
 
-    user = factory.SubFactory(UserFactory)
+    user = factory.SubFactory(UserFactory, email=factory.SelfAttribute("..email"))
+    email = factory.Faker("email")
     firstname = factory.Faker("first_name", locale="fr_FR")
     lastname = factory.Faker("last_name", locale="fr_FR")
     poste = factory.fuzzy.FuzzyChoice(PosteType)
