3c89a38687bb1e1257692cc8b1490261aeec282d
Etienne de la Broïse <etienne.de-la-broise.ext@beta.gouv.fr>
2026-05-25 12:37:48 +0200

    refactor(nuxt): move event options logic to a plugin

---
+++ nuxt/plugins/event.js
@@ -0,0 +1,47 @@
+
+import CCEvents from '@/assets/data/events/CC_events.json'
+import PLUEvents from '@/assets/data/events/PLU_events.json'
+import SCoTEvents from '@/assets/data/events/SCOT_events.json'
+
+export function getDocumentTypeEvents (documentType) {
+  if (!documentType) {
+    return []
+  }
+  if (documentType.match(/i|H|M/)) {
+    return PLUEvents
+  }
+  switch (documentType) {
+    case 'PLU':
+    case 'POS':
+      return PLUEvents
+    case 'SCOT':
+    case 'SD':
+      return SCoTEvents
+    case 'CC':
+      return CCEvents
+  }
+}
+
+export function getProcedureEventsScope (procedure) {
+  if (!procedure) {
+    return 'aucun'
+  }
+  switch (procedure.type) {
+    case 'Elaboration':
+    case 'Révision':
+      return procedure.current_perimetre.length > 1 && procedure.doc_type !== 'CC' ? 'ppi' : 'pp'
+    case 'Mise à jour':
+      return 'mj'
+    case 'Mise en compatibilité':
+      return 'mc'
+    case 'Modification':
+      return procedure.started_before_huwart_law ? 'm' : 'mlh'
+    case 'Modification simplifiée':
+      return 'ms'
+    case 'Révision allégée (ou RMS)':
+    case 'Révision à modalité simplifiée ou Révision allégée':
+      return 'rms'
+    default:
+      return 'aucun'
+  }
+}
