ac5aa5866e4f3e42819b375eff6469d246ad4b25
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-06-08 14:32:21 +0200

    Django CORS: fix broken local environment

--- django/config/settings/dev.py
+++ django/config/settings/dev.py
@@ -1,3 +1,5 @@
+from corsheaders.defaults import default_headers
+
 # ruff: noqa: F405 F403
 from config.settings.base import *  # NOSONAR (S2208)
 
@@ -35,3 +37,7 @@ SESSION_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOW_HEADERS = [
+    *default_headers,
+    "supabase-authorization",
+]
