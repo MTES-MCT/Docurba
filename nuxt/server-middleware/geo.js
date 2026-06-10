fa6f6658fcf617481a618b9f4fcb61ad41c25f95
Etienne de la Broïse <etienne.de-la-broise.ext@beta.gouv.fr>
2026-06-10 15:21:31 +0200

    chore(nuxt): remove unused jsons & endpoints

--- nuxt/server-middleware/geo.js
+++ nuxt/server-middleware/geo.js
@@ -10,27 +10,9 @@ app.get('/communes', (req, res) => {
   res.status(200).send(communes)
 })
 
-app.get('/communes/:code', (req, res) => {
-  const commune = geo.getCommune(req.params.code)
-  if (commune) {
-    res.status(200).send(commune)
-  } else {
-    res.status(404).send(null)
-  }
-})
-
 app.get('/intercommunalites', (req, res) => {
   const intercommunalites = geo.getIntercommunalites(req.query)
   res.status(200).send(intercommunalites)
-})
-
-app.get('/intercommunalites/:code', (req, res) => {
-  const intercommunalite = geo.getIntercommunalite(req.params.code)
-  if (intercommunalite) {
-    res.status(200).send(intercommunalite)
-  } else {
-    res.status(404).send(null)
-  }
 })
 
 app.get('/collectivites', (req, res) => {
