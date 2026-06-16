fb08e75ee0627f29cfba504bb7b7e8f6d16a077c
Etienne de la Broïse <etienne.de-la-broise.ext@beta.gouv.fr>
2026-06-16 16:02:10 +0200

    feat(nuxt): migrate /api/communes endpoints to django

--- django/docurba/internal_api/views.py
+++ django/docurba/internal_api/views.py
@@ -81,6 +81,7 @@ class CommuneViewSet(viewsets.ReadOnlyModelViewSet):
     )
     serializer_class = CommuneSerializer
     filterset_class = custom_filters.CommuneFilter
+    lookup_field = "code_insee_unique"
 
 
 class EventTypeViewSet(viewsets.ReadOnlyModelViewSet):
