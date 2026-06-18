5de8017dc9715b775bf44d9bc9837b123d86fd3d
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2026-06-18 15:38:33 +0200

    Clean unused SQL functions

--- django/docurba/core/migrations/sql_statements/create_table_procedures_perimetres.sql
+++ django/docurba/core/migrations/sql_statements/create_table_procedures_perimetres.sql
@@ -10,16 +10,6 @@ CREATE TABLE public.procedures_perimetres (
     commune_id text GENERATED ALWAYS AS (((collectivite_code || '_'::text) || collectivite_type)) STORED NOT NULL
 );
 
-CREATE FUNCTION public.perimetre_by_procedures_ids_and_insee_codes(procedures_ids json, insee_codes json) RETURNS void
-    LANGUAGE sql
-    AS $$
-        UPDATE procedures_perimetres
-        SET opposable = true
-        WHERE procedure_id::text IN (SELECT value FROM jsonb_array_elements_text(procedures_ids::jsonb))
-        AND collectivite_code::text IN (SELECT value FROM jsonb_array_elements_text(insee_codes::jsonb))
-    $$
-;
-
 CREATE FUNCTION procedures_by_collectivites(codes json)
 RETURNS SETOF record
 AS $$
@@ -35,23 +25,6 @@ AS $$
     ) GROUP BY p.id;
 $$ LANGUAGE sql
 SECURITY INVOKER;
-
-CREATE FUNCTION public.procedures_principales_by_collectivites(codes json)
-    RETURNS SETOF record
-    LANGUAGE sql
-    AS $$
-        SELECT p.*, array_agg(pp.*) AS procedures_perimetres
-        FROM procedures p
-        LEFT JOIN procedures_perimetres pp ON p.id = pp.procedure_id
-        WHERE ((p.id IN (
-            SELECT procedure_id
-            FROM procedures_perimetres
-            WHERE collectivite_code IN (
-                SELECT json_array_elements_text(codes)
-            )
-        )) AND p.is_principale = true AND p.status IN ('opposable', 'en cours')) GROUP BY p.id;
-    $$
-;
 
 ALTER TABLE ONLY public.procedures_perimetres
     ADD CONSTRAINT procedures_perimetres_pkey PRIMARY KEY (id);
@@ -97,7 +70,3 @@ GRANT ALL ON FUNCTION public.procedures_by_collectivites(codes json) TO service_
 GRANT SELECT,INSERT,REFERENCES,DELETE,TRIGGER,TRUNCATE,UPDATE ON TABLE public.procedures_perimetres TO anon;
 GRANT SELECT,INSERT,REFERENCES,DELETE,TRIGGER,TRUNCATE,UPDATE ON TABLE public.procedures_perimetres TO authenticated;
 GRANT SELECT,INSERT,REFERENCES,DELETE,TRIGGER,TRUNCATE,UPDATE ON TABLE public.procedures_perimetres TO service_role;
-
-GRANT ALL ON FUNCTION public.perimetre_by_procedures_ids_and_insee_codes(procedures_ids json, insee_codes json) TO anon;
-GRANT ALL ON FUNCTION public.perimetre_by_procedures_ids_and_insee_codes(procedures_ids json, insee_codes json) TO authenticated;
-GRANT ALL ON FUNCTION public.perimetre_by_procedures_ids_and_insee_codes(procedures_ids json, insee_codes json) TO service_role;
--- django/docurba/core/migrations/sql_statements/drop_table_procedures.sql
+++ django/docurba/core/migrations/sql_statements/drop_table_procedures.sql
@@ -5,6 +5,3 @@ ALTER TABLE ONLY public.procedures_validations
     DROP CONSTRAINT procedures_validations_procedure_id_fkey;
 
 DROP TABLE public.procedures;
-
-DROP FUNCTION public.procedures_by_insee_codes;
-DROP FUNCTION public.procedures_by_sudocuh_ids;
