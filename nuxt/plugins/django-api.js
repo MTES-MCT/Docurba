1a57d0fe778fe802bf1b77766bcdfdb1aac1c8ca
Etienne de la Broïse <etienne.de-la-broise.ext@beta.gouv.fr>
2026-06-22 14:07:53 +0200

    fix(nuxt): use the right variable & value for the django api loop

--- nuxt/plugins/django-api.js
+++ nuxt/plugins/django-api.js
@@ -1,17 +1,15 @@
-/* eslint-disable unicorn/no-anonymous-default-export */
-import Qs from "qs"
+import Qs from 'qs'
 
 export default ({ $axios, $user }, inject) => {
   const djangoAxios = $axios.create({
     baseURL: process.env.DJANGO_API_BASE_URL,
-    paramsSerializer: params => Qs.stringify(params, {arrayFormat: 'repeat', encode: false})
+    paramsSerializer: params => Qs.stringify(params, { arrayFormat: 'repeat', encode: false })
   })
   inject('djangoApi', {
     async get (path, parameters) {
-      // eslint-disable-next-line n/prefer-global/process
       const requestOpts = { params: parameters }
       if ($user.supabase_access_token) {
-        requestOpts.headers = { "Supabase-Authorization": $user.supabase_access_token }
+        requestOpts.headers = { 'Supabase-Authorization': $user.supabase_access_token }
       }
       const { data: responseData } = await djangoAxios.get(path, requestOpts)
       if (responseData.results === undefined) {
@@ -22,10 +20,10 @@ export default ({ $axios, $user }, inject) => {
       results.push(...responseData.results)
       if (responseData.num_pages > 1) {
         let nextUrl = responseData.next
-        while (nextUrl !== undefined) {
+        while (nextUrl !== null) {
           const { data: response } = await $axios.get(nextUrl)
           results.push(...response.results)
-          nextUrl = results.next
+          nextUrl = response.next
         }
       }
 
