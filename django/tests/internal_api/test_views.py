ca5829950a57abeeb5195eb650b17b0d4586a0f3
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-06-23 15:49:57 +0200

    Django internal API: add a test
    Ref 74b99f4ec632e8d76dec28aff7316e931c8cde8b

--- django/tests/internal_api/test_views.py
+++ django/tests/internal_api/test_views.py
@@ -307,6 +307,19 @@ class TestCommunesAPI:
                 BASE_QUERIES_COUNT + 1,
                 id="type_commune",
             ),
+            pytest.param(
+                {"code": "13150"},
+                [
+                    {
+                        "code": "13150",
+                        "type": "COM",
+                        "intitule": "Commune 1",
+                        "regionCode": "93",
+                        "departementCode": "13",
+                    },
+                ],
+                id="code_insee",
+            ),
         ],
     )
     def test_list(
