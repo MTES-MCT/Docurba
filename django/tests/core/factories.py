051b1f3fd9cb693e2df3f735ddf917f8016f32bc
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-06-29 17:51:32 +0200

    Django internal API: avoid an N+1
    When several INSEE codes are passed to CommuneViewSet,
    filters.AllValuesMultipleFilter performs one DB request
    per INSEE code, leading to N+1 requests.
    
    Storing INSEE codes in memory is a tradeoff to avoid
    N+1 as it forces us to warm up the cache once
    when using assertNumQueries.
    I did not find how to do it differently.

--- django/tests/core/factories.py
+++ django/tests/core/factories.py
@@ -24,6 +24,7 @@ from docurba.core.models import (
     TypeCollectivite,
     TypeDocument,
 )
+from docurba.internal_api.filters import get_insee_codes_choices
 from tests.users.factories import ProfileFactory
 
 REGIONS = {
@@ -111,6 +112,11 @@ class CommuneFactory(factory.django.DjangoModelFactory):
             ]
         ),
     )
+
+    @classmethod
+    def refresh_cache(cls) -> None:
+        get_insee_codes_choices.cache_clear()
+        get_insee_codes_choices()
 
 
 class CollectiviteFactory(factory.django.DjangoModelFactory):
