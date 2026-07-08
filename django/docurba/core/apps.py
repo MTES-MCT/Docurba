d2be6defca09ddcf318d55b669e922079692750c
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2026-07-08 16:20:29 +0200

    Update event_types after post_migrate signal

--- django/docurba/core/apps.py
+++ django/docurba/core/apps.py
@@ -6,6 +6,8 @@ from django.apps import AppConfig
 from django.conf import settings
 from django.db import connection, models, transaction
 
+from docurba.utils.enums import DocurbaEnvironment
+
 
 class CoreAppConfig(AppConfig):
     name = "docurba.core"
@@ -14,6 +16,8 @@ class CoreAppConfig(AppConfig):
     def ready(self) -> None:
         super().ready()
         models.signals.post_migrate.connect(create_topics, sender=self)
+        if settings.DOCURBA_ENVIRONMENT != DocurbaEnvironment.TEST:
+            models.signals.post_migrate.connect(create_event_types, sender=self)
 
 
 def create_topics(*args: list[str, Any], **kwargs: dict[str, Any]) -> None:  # noqa: ARG001
@@ -28,4 +32,19 @@ def create_topics(*args: list[str, Any], **kwargs: dict[str, Any]) -> None:  # n
                 Topic.objects.update_or_create(
                     pk=topic["pk"],
                     defaults=topic["fields"],
+                )
+
+
+def create_event_types(*args: list[str, Any], **kwargs: dict[str, Any]) -> None:  # noqa: ARG001
+    from docurba.core.models import EventType  # noqa: PLC0415
+
+    if EventType._meta.db_table in connection.introspection.table_names():  # noqa: SLF001
+        json_path = pathlib.Path(settings.APPS_DIR) / "core/data/event_types.json"
+        with json_path.open("rb") as fp:
+            event_types = json.load(fp)
+        with transaction.atomic():
+            for event_type in event_types:
+                EventType.all_objects.update_or_create(
+                    pk=event_type["pk"],
+                    defaults=event_type["fields"],
                 )
