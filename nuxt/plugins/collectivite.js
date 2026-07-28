442d72637410af2eea4b33746824624e7c4e19f1
Etienne de la Broïse <etienne.de-la-broise.ext@beta.gouv.fr>
2026-07-28 10:39:04 +0200

    fix(nuxt): handle long lists of codes siren on the collectivites query

--- nuxt/plugins/collectivite.js
+++ nuxt/plugins/collectivite.js
@@ -14,7 +14,27 @@ export async function getCollectivite (api, id, params) {
 }
 
 export async function listCollectivites (api, params) {
-  return (await api.get('/api-internes/collectivites/', params)).map(parseCollectivite)
+  if (!params.codes_siren || params.codes_siren.length < 100) {
+    return (await api.get('/api-internes/collectivites/', params)).map(parseCollectivite)
+  }
+
+  const collectivitesQueries = []
+
+  for (let i = 0; i < params.codes_siren.length; i += 100) {
+    collectivitesQueries.push(api.get('/api-internes/collectivites/', {
+      ...params,
+      codes_siren: params.codes_siren.slice(i, i + 100)
+    }))
+  }
+
+  return (await Promise.all(collectivitesQueries))
+    .flatMap(c => c.map(parseCollectivite))
+    .sort((a, b) => {
+      const aCode = Number(a.code)
+      const bCode = Number(b.code)
+
+      return aCode === bCode ? 0 : aCode > bCode ? 1 : -1
+    })
 }
 
 function parseCollectivite (collectivite) {
