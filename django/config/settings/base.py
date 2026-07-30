e69c8a7af98e621258e9afe6effaceb55704aa63
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-07-30 11:35:02 +0200

    Remove default settings
    They were set to default values so no need to specify it.

--- django/config/settings/base.py
+++ django/config/settings/base.py
@@ -130,10 +130,6 @@ LANGUAGE_CODE = "fr-fr"
 
 TIME_ZONE = "UTC"
 
-USE_I18N = True
-
-USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
