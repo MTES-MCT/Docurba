19543a16bbbb582ebdc8ba8d240a0d53a432823b
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-07-30 11:38:40 +0200

    Use the correct time zone

--- django/config/settings/base.py
+++ django/config/settings/base.py
@@ -127,8 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = "fr-fr"
-
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Paris"
 
 
 # Static files (CSS, JavaScript, Images)
