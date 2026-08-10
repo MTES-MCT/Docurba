b425f90c0ef1ca70a43d9bd3ba6d130e97dd95dd
Etienne de la Broïse <etienne.de-la-broise.ext@beta.gouv.fr>
2026-08-10 17:06:39 +0200

    feat(nuxt): display an env banner on non-production envs

---
+++ nuxt/components/Layouts/EnvBanner.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="visible" class="env-banner">
+    {{ label }}
+  </div>
+</template>
+
+<script>
+import { ENVIRONMENT, ENVIRONMENT_LABEL } from '@/plugins/environement'
+
+export default {
+  computed: {
+    label () {
+      return ENVIRONMENT_LABEL[process.env.USER_ENVIRONMENT]
+    },
+    visible () {
+      return this.environment !== ENVIRONMENT.PRODUCTION
+    }
+  }
+}
+</script>
+
+<style>
+.env-banner {
+  background-color: var(--v-primary-base);
+  color: #fff;
+  display: grid;
+  left: 0;
+  padding: .5rem 4.6875rem .5rem .8125rem;
+  pointer-events: none;
+  position: absolute;
+  text-align: center;
+  top: 3.375rem;
+  transform: rotate(-22.5deg);
+  transform-origin: bottom left;
+  width: 15.25rem;
+}
+</style>
---
+++ nuxt/plugins/environement.js
@@ -0,0 +1,13 @@
+export const ENVIRONMENT = {
+  DEMO: 'demo',
+  DEVELOPMENT: 'development',
+  PRODUCTION: 'production',
+  REVIEW_APP: 'review_app'
+}
+
+export const ENVIRONMENT_LABEL = {
+  [ENVIRONMENT.DEMO]: 'Démonstration',
+  [ENVIRONMENT.DEVELOPMENT]: 'Développement',
+  [ENVIRONMENT.PRODUCTION]: 'Production',
+  [ENVIRONMENT.REVIEW_APP]: 'Recette jetable'
+}
