eb64177dd7de33324abd749247b0c6b083c7c03a
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2026-09-21 17:05:16 +0200

    Remove nuxt api unused endpoints : /api/urba
    - /exports/prescriptions
    - remove related modules (exportMaps/prescriptions.js)

--- nuxt/server-middleware/urba.js
+++ nuxt/server-middleware/urba.js
@@ -3,41 +3,13 @@ import { AsyncParser } from '@json2csv/node'
 
 // GEO DATA
 import express from 'express'
-import { mapValues, get, uniq } from 'lodash'
 import allCommunes from './Data/referentiels/communes.json'
 import groupements from './Data/referentiels/groupements.json'
-
-import supabase from './modules/supabase.js'
-
-// exports maps
-import prescriptionsMap from './modules/exportMaps/prescriptions.js'
 
 const csvParser = new AsyncParser()
 
 const app = express()
 app.use(express.json())
-
-app.get('/exports/prescriptions', async (req, res) => {
-  const { data: prescriptions } = await supabase.from('prescriptions').select('*')
-
-  const userIds = uniq(prescriptions.map(p => p.user_id).filter(id => !!id))
-  const { data: profiles } = await supabase.from('profiles').select('*').in('user_id', userIds)
-
-  prescriptions.forEach((p) => {
-    p.profile = profiles.find(profile => p.user_id === profile.user_id)
-  })
-
-  if (req.query.csv) {
-    const mappedPrescriptions = prescriptions.map((prescription) => {
-      return mapValues(prescriptionsMap, key => get(prescription, key, ''))
-    })
-
-    const csv = await csvParser.parse(mappedPrescriptions).promise()
-    res.status(200).attachment('prescriptions.csv').send(csv)
-  } else {
-    res.status(200).send(prescriptions)
-  }
-})
 
 app.get('/exports/gpu/maillages', async (req, res) => {
   const groupementsCompetents = groupements.filter(
