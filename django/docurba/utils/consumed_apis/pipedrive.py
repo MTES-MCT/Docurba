c76f52cce42f8d38da5a53088bce98111e633ec8
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2026-09-25 15:58:54 +0200

    Django: add a Pipedrive API client
    This is a first step towards the integration of the Pipedrive
    synchronization feature.
    As it's not fully defined, we just "move" the existing behaviour to
    Django without modifying it.
    The way exceptions are currently handled (or not) may be a real problem
    but I don't know if it occurs a lot and what the correct behaviour
    should be.

--- django/docurba/utils/consumed_apis/pipedrive.py
+++ django/docurba/utils/consumed_apis/pipedrive.py
@@ -18,10 +18,9 @@
 # User Followers API
 # Search API
 
-# TODO: add tenacity
-
 import dataclasses
 import logging
+from typing import Self
 
 import httpx
 from django.conf import settings
@@ -29,7 +28,7 @@ from django.conf import settings
 logger = logging.getLogger(__name__)
 
 
-class PipedriveException(Exception):
+class PipedriveHTTPError(Exception):
     pass
 
 
@@ -50,43 +49,61 @@ class PipedriveApiClient:
             headers={"x-api-token": settings.PIPEDRIVE_TOKEN},
         )
 
-    def __enter__(self):
+    def __enter__(self) -> Self:
         self.client.__enter__()
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, type, value, traceback) -> None:  # noqa: A002, ANN001
         self.client.__exit__(type, value, traceback)
 
-    def _request(self, route, params, *, method="GET") -> httpx.Response:
+    def _request(
+        self, route: str, params: dict, *, method: str = "GET"
+    ) -> httpx.Response:
         try:
             response = self.client.request(
                 method, route, params=params, timeout=httpx.Timeout(5, read=60)
             ).raise_for_status()
         except httpx.HTTPError as exc:
-            logger.exception("pipedrive exception")
-            raise PipedriveException from exc
+            logger.exception("Pipedrive exception")
+            raise PipedriveHTTPError from exc
 
         return response
 
-    def search_organizations(self, term, **params) -> list:
+    def search_organizations(self, term: str, **params: dict) -> list | None:
         # https://developers.pipedrive.com/docs/api/v1/Organizations
         params = {
             "term": term,  # mandatory
         } | params
-        return self._request("/organizations/search", params).json()["data"]["items"]
+        results = self._request("/organizations/search", params).json()
+        if not results:
+            return None
+        if "data" not in results or "items" not in results["data"]:
+            raise KeyError
+        return results["data"]["items"]
 
-    def find_organization_by_name(self, name: str) -> dict:
+    def deals(self, **params: dict) -> dict | None:
+        # https://developers.pipedrive.com/docs/api/v1/Deals
+        results = self._request("/deals", params).json()
+        if not results:
+            return None
+        if "data" not in results:
+            raise KeyError
+        return results["data"]
+
+    def update_deal(self, deal_id: int, **params: dict) -> dict | None:
+        # https://developers.pipedrive.com/docs/api/v1/Deals
+        response = self._request(f"/deals/{deal_id}", params, method="PATCH").json()
+        if "data" not in response:
+            raise KeyError
+        return response["data"]
+
+    def find_organization_by_name(self, name: str) -> dict | None:
         results = self.search_organizations(term=name, fields=["name"])
-        return results[0]["item"] if results else None
+        if not results:
+            return []
+        if "item" not in results[0]:
+            raise KeyError
+        return results[0]["item"]
 
-    def deals(self, **params):
-        # https://developers.pipedrive.com/docs/api/v1/Deals
-        return self._request("/deals", params).json()
-
-    def get_organization_deals(self, organization_id):
-        results = self.deals(org_id=organization_id)
-        return results["data"] if results else None
-
-    def update_deal(self, deal_id, **params) -> None:
-        # https://developers.pipedrive.com/docs/api/v1/Deals
-        return self._request("/organizations", params, method="PATCH").json()
+    def get_organization_deals(self, organization_id: str) -> dict | None:
+        return self.deals(org_id=organization_id)
--- django/tests/utils/consumed_apis/test_pipedrive.py
+++ django/tests/utils/consumed_apis/test_pipedrive.py
@@ -1,10 +1,10 @@
-
-# import tenacity
+import pytest
+from respx import MockRouter
 
 from docurba.utils.consumed_apis.pipedrive import PipedriveApiClient
 
 
-def test_search_organizations(respx_mock) -> None:
+def test_search_organizations(respx_mock: MockRouter) -> None:
     expected = [
         {
             "result_score": 0.35513002,
@@ -35,7 +35,7 @@ def test_search_organizations(respx_mock) -> None:
     assert result == expected
 
 
-def test_search_organizations_no_result(respx_mock) -> None:
+def test_search_organizations_no_result(respx_mock: MockRouter) -> None:
     respx_mock.get(
         "https://fake-pipedrive.fr/organizations/search?term=DDT%2001&fields=name",
     ).respond(
@@ -51,7 +51,7 @@ def test_search_organizations_no_result(respx_mock) -> None:
     assert result == []
 
 
-def test_find_organization_by_name(respx_mock) -> None:
+def test_find_organization_by_name(respx_mock: MockRouter) -> None:
     expected = {
         "id": 1,
         "type": "organization",
@@ -85,23 +85,7 @@ def test_find_organization_by_name(respx_mock) -> None:
     assert result == expected
 
 
-def test_find_organization_by_name_no_result(respx_mock) -> None:
-    respx_mock.get(
-        "https://fake-pipedrive.fr/organizations/search?term=DDT%2001&fields=name",
-    ).respond(
-        200,
-        json={
-            "success": True,
-            "data": {"items": []},
-            "additional_data": {"next_cursor": None},
-        },
-    )
-    with PipedriveApiClient() as client:
-        result = client.find_organization_by_name(name="DDT 01")
-    assert result is None
-
-
-def test_get_organization_deals(respx_mock) -> None:
+def test_get_organization_deals(respx_mock: MockRouter) -> None:
     expected = [
         {
             "id": 51,
@@ -159,3 +143,124 @@ def test_get_organization_deals(respx_mock) -> None:
     with PipedriveApiClient() as client:
         result = client.get_organization_deals(organization_id=1)
     assert result == expected
+
+
+def test_update_deal(respx_mock: MockRouter) -> None:
+    expected = {
+        "id": 1,
+        "title": "DDT Test",
+        "creator_user_id": 34070131,
+        "value": 0.0,
+        "person_id": 1,
+        "org_id": 1,
+        "stage_id": 3,
+        "currency": "EUR",
+        "add_time": "2026-09-25T09:36:04Z",
+        "update_time": "2026-09-25T09:42:23Z",
+        "status": "open",
+        "probability": None,
+        "lost_reason": None,
+        "visible_to": 3,
+        "close_time": None,
+        "pipeline_id": 1,
+        "won_time": None,
+        "lost_time": None,
+        "stage_change_time": "2026-09-25T09:42:23Z",
+        "local_won_date": None,
+        "local_lost_date": None,
+        "local_close_date": None,
+        "expected_close_date": None,
+        "custom_fields": None,
+        "owner_id": 34070131,
+        "label_ids": [],
+        "is_deleted": False,
+        "origin": "ManuallyCreated",
+        "origin_id": None,
+        "channel": None,
+        "channel_id": None,
+        "is_archived": None,
+        "archive_time": None,
+    }
+
+    respx_mock.patch(
+        "https://fake-pipedrive.fr/deals/1",
+    ).respond(
+        200,
+        json={
+            "success": True,
+            "data": expected,
+        },
+    )
+    with PipedriveApiClient() as client:
+        result = client.update_deal(deal_id=1)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ("endpoint", "method", "method_args"),
+    [
+        pytest.param(
+            "organizations/search?term=DDT%2001&fields=name",
+            "find_organization_by_name",
+            {"name": "DDT 01"},
+            id="find_organization_by_name",
+        ),
+        pytest.param(
+            "organizations/search?term=DDT%2001&fields=name",
+            "search_organizations",
+            {"term": "DDT 01", "fields": "name"},
+            id="search_organizations",
+        ),
+    ],
+)
+def test_no_result__item_expected_in_dict(
+    respx_mock: MockRouter, endpoint: str, method: str, method_args: dict
+) -> None:
+    respx_mock.get(
+        f"https://fake-pipedrive.fr/{endpoint}",
+    ).respond(
+        200,
+        json={
+            "success": True,
+            "data": {"items": []},
+            "additional_data": {"next_cursor": None},
+        },
+    )
+    with PipedriveApiClient() as client:
+        result = getattr(client, method)(**method_args)
+    assert result == []
+
+
+@pytest.mark.parametrize(
+    ("endpoint", "method", "method_args"),
+    [
+        pytest.param(
+            "deals?org_id=1",
+            "deals",
+            {"org_id": 1},
+            id="deals",
+        ),
+        pytest.param(
+            "deals?org_id=1",
+            "get_organization_deals",
+            {"organization_id": 1},
+            id="get_organization_deals",
+        ),
+    ],
+)
+def test_no_result(
+    respx_mock: MockRouter, endpoint: str, method: str, method_args: dict
+) -> None:
+    respx_mock.get(
+        f"https://fake-pipedrive.fr/{endpoint}",
+    ).respond(
+        200,
+        json={
+            "success": True,
+            "data": [],
+            "additional_data": {"next_cursor": None},
+        },
+    )
+    with PipedriveApiClient() as client:
+        result = getattr(client, method)(**method_args)
+    assert result == []
